024824126ab809b83db8f11886f1528cb45afbd5
Daniel Bosen <daniel.bosen@burda.com>
2019-05-14 17:50:10 +0200

    use thunder-distribution dir in tests

--- scripts/travis/03-build-thunder-drush.sh
+++ scripts/travis/03-build-thunder-drush.sh
@@ -4,15 +4,15 @@ cd ${THUNDER_DIST_DIR}
 
 # Build drupal + thunder from makefile
 drush make --concurrency=5 drupal-org-core.make ${TEST_DIR}/docroot -y
-mkdir ${TEST_DIR}/docroot/profiles/thunder
+mkdir ${TEST_DIR}/docroot/profiles/thunder-distribution
 shopt -s extglob
-rsync -a . ${TEST_DIR}/docroot/profiles/thunder --exclude docroot
+rsync -a . ${TEST_DIR}/docroot/profiles/thunder-distribution --exclude docroot
 
-drush make -y --no-core ${TEST_DIR}/docroot/profiles/thunder/drupal-org.make ${TEST_DIR}/docroot/profiles/thunder
+drush make -y --no-core ${TEST_DIR}/docroot/profiles/thunder-distribution/drupal-org.make ${TEST_DIR}/docroot/profiles/thunder-distribution
 
 # Get development branch of Thunder Admin theme (to use same admin theme as for composer build)
-rm -rf ${TEST_DIR}/docroot/profiles/thunder/themes/thunder_admin
-git clone --depth 1 --single-branch --branch ${THUNDER_ADMIN_BRANCH} https://github.com/BurdaMagazinOrg/theme-thunder-admin.git ${TEST_DIR}/docroot/profiles/thunder/themes/thunder_admin
+rm -rf ${TEST_DIR}/docroot/profiles/thunder-distribution/themes/thunder_admin
+git clone --depth 1 --single-branch --branch ${THUNDER_ADMIN_BRANCH} https://github.com/BurdaMagazinOrg/theme-thunder-admin.git ${TEST_DIR}/docroot/profiles/thunder-distribution/themes/thunder_admin
 
 composer install --working-dir=${TEST_DIR}/docroot
 composer run-script drupal-phpunit-upgrade --working-dir=${TEST_DIR}/docroot
