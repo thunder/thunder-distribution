08140f25b3c092deaf4b682df2e391c731c30d47
Mladen Todorovic <mladen.todorovic@burda.com>
2019-05-24 10:42:57 +0200

    Adjusted script to upload files to s3

--- scripts/travis/07-upload-artifacts-to-s3.sh
+++ scripts/travis/07-upload-artifacts-to-s3.sh
@@ -6,24 +6,24 @@
 pip install --user awscli
 
 # Package database
-gzip "${DEPLOYMENT_DUMP_FILE}"
-DB_ASSET_FILE="db-dump-${TRAVIS_BUILD_ID}.gz"
-mv "${DEPLOYMENT_DUMP_FILE}.gz" "${DB_ASSET_FILE}"
-
-# Package project
-
-cd "${TEST_DIR}"
+DB_ARTIFACT_FILE_NAME="${TRAVIS_BUILD_ID}-db-dump.gz"
+DB_ARTIFACT_FILE="${THUNDER_DIST_DIR}/../${DB_ARTIFACT_FILE_NAME}"
+gzip < "${DEPLOYMENT_DUMP_FILE}" > "${DB_ARTIFACT_FILE}"
 
 # Cleanup project
+cd "${TEST_DIR}"
 composer install --no-dev
 rm -rf "${TEST_DIR}/docroot/sites/default/files/*"
 find "${TEST_DIR}" -type d -name ".git" | xargs rm -rf
 find "${THUNDER_DIST_DIR}" -type d -name ".git" | xargs rm -rf
 
 # Make zip for package
-PROJECT_ASSET_FILE="thunder-${TRAVIS_BUILD_ID}.tar.gz"
-tar -czhf "${PROJECT_ASSET_FILE}" "${TEST_DIR}"
+PROJECT_ARTIFACT_FILE_NAME="${TRAVIS_BUILD_ID}-thunder.tar.gz"
+PROJECT_ARTIFACT_FILE="${THUNDER_DIST_DIR}/../${PROJECT_ARTIFACT_FILE_NAME}"
+cd "${TEST_DIR}" && tar -czhf "${PROJECT_ARTIFACT_FILE}" .
 
-# Upload files to S3
-# TODO: implement it!
+# Upload files to S3 bucket
+AWS_ACCESS_KEY_ID="${ARTIFACTS_KEY}" AWS_SECRET_ACCESS_KEY="${ARTIFACTS_SECRET}" aws s3 cp "${DB_ARTIFACT_FILE}" "s3://thunder-builds/${DB_ARTIFACT_FILE_NAME}"
+AWS_ACCESS_KEY_ID="${ARTIFACTS_KEY}" AWS_SECRET_ACCESS_KEY="${ARTIFACTS_SECRET}" aws s3 cp "${PROJECT_ARTIFACT_FILE}" "s3://thunder-builds/${PROJECT_ARTIFACT_FILE_NAME}"
+
 exit 1
