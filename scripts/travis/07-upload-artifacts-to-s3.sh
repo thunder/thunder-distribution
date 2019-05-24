85c8fa2951c68697788a3a9db245dff5dde6cd01
Mladen Todorovic <mladen.todorovic@burda.com>
2019-05-24 11:27:22 +0200

    Added reuse of built project artifact

--- scripts/travis/07-upload-artifacts-to-s3.sh
+++ scripts/travis/07-upload-artifacts-to-s3.sh
@@ -2,12 +2,7 @@
 #
 # Package and upload built Thunder project and database
 
-# Install AWS CLI
-pip install --user awscli
-
 # Package database
-DB_ARTIFACT_FILE_NAME="${TRAVIS_BUILD_ID}-db-dump.gz"
-DB_ARTIFACT_FILE="${THUNDER_DIST_DIR}/../${DB_ARTIFACT_FILE_NAME}"
 gzip < "${DEPLOYMENT_DUMP_FILE}" > "${DB_ARTIFACT_FILE}"
 
 # Cleanup project
@@ -18,12 +13,8 @@ find "${TEST_DIR}" -type d -name ".git" | xargs rm -rf
 find "${THUNDER_DIST_DIR}" -type d -name ".git" | xargs rm -rf
 
 # Make zip for package
-PROJECT_ARTIFACT_FILE_NAME="${TRAVIS_BUILD_ID}-thunder.tar.gz"
-PROJECT_ARTIFACT_FILE="${THUNDER_DIST_DIR}/../${PROJECT_ARTIFACT_FILE_NAME}"
 cd "${TEST_DIR}" && tar -czhf "${PROJECT_ARTIFACT_FILE}" .
 
 # Upload files to S3 bucket
 AWS_ACCESS_KEY_ID="${ARTIFACTS_KEY}" AWS_SECRET_ACCESS_KEY="${ARTIFACTS_SECRET}" aws s3 cp "${DB_ARTIFACT_FILE}" "s3://thunder-builds/${DB_ARTIFACT_FILE_NAME}"
 AWS_ACCESS_KEY_ID="${ARTIFACTS_KEY}" AWS_SECRET_ACCESS_KEY="${ARTIFACTS_SECRET}" aws s3 cp "${PROJECT_ARTIFACT_FILE}" "s3://thunder-builds/${PROJECT_ARTIFACT_FILE_NAME}"
-
-exit 1
