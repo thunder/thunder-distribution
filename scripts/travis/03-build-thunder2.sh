3c44422e2ef2461c46df5c57989c3d9b625f168e
Christian Fritsch <chr.fritsch@gmx.net>
2019-05-27 14:42:46 +0200

    Update liveblog

--- scripts/travis/03-build-thunder2.sh
+++ scripts/travis/03-build-thunder2.sh
@@ -4,5 +4,5 @@ cd ${THUNDER_DIST_DIR}
 composer create-project BurdaMagazinOrg/thunder-project:2.x ${TEST_DIR} --stability dev --no-interaction --no-install
 
 cd ${TEST_DIR}
-
+composer require "drupal/liveblog:^1.0-alpha2" --no-progress --no-update
 COMPOSER_MEMORY_LIMIT=-1 composer update --no-progress --prefer-lowest
