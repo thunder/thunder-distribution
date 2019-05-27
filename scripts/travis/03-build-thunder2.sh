2ee173838d11351e666d034c10b7d9f9c866405b
Christian Fritsch <chr.fritsch@gmx.net>
2019-05-27 13:55:19 +0200

    Fix package

--- scripts/travis/03-build-thunder2.sh
+++ scripts/travis/03-build-thunder2.sh
@@ -6,4 +6,4 @@ composer create-project BurdaMagazinOrg/thunder-project:2.x ${TEST_DIR} --stabil
 cd ${TEST_DIR}
 
 composer config repositories.thunder path ${THUNDER_DIST_DIR}
-composer require "BurdaMagazinOrg/thunder-distribution:*" "mglaman/phpstan-drupal" "phpstan/phpstan:0.11.6" "nette/di:*@stable" "phpstan/phpstan-deprecation-rules" --no-progress
+composer require "BurdaMagazinOrg/thunder:*" "mglaman/phpstan-drupal" "phpstan/phpstan:0.11.6" "nette/di:*@stable" "phpstan/phpstan-deprecation-rules" --no-progress
