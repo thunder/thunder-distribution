722a7776db3988197ae52bb1e061faaa6e30e9e3
Mladen Todorovic <mladen.todorovic@burda.com>
2019-05-27 09:56:08 +0200

    Fixed reuse of database

--- scripts/travis/04-use-database-artifact.sh
+++ scripts/travis/04-use-database-artifact.sh
@@ -6,8 +6,9 @@
 AWS_ACCESS_KEY_ID="${ARTIFACTS_KEY}" AWS_SECRET_ACCESS_KEY="${ARTIFACTS_SECRET}" aws s3 cp "s3://thunder-builds/${DB_ARTIFACT_FILE_NAME}" "${DB_ARTIFACT_FILE}"
 
 # Unzip to deployment file. Deployment file should be provided for other scripts to work properly.
-gunzip "${DB_ARTIFACT_FILE}" > "${DEPLOYMENT_DUMP_FILE}"
+gunzip < "${DB_ARTIFACT_FILE}" > "${DEPLOYMENT_DUMP_FILE}"
 
 # Import database
 cd "${TEST_DIR}/docroot"
+drush -y sql-create
 drush -y sql-cli < ${DEPLOYMENT_DUMP_FILE}
