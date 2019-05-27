bfb69d4f764ed54ba8143240d7f48ee028073e96
Christian Fritsch <chr.fritsch@gmx.net>
2019-05-27 16:11:06 +0200

    Foo

--- scripts/travis/04-install-thunder.sh
+++ scripts/travis/04-install-thunder.sh
@@ -5,7 +5,6 @@
 install_thunder() {
     cd $1
 
-    ls -al
     /usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" drush si thunder -vvv --db-url=mysql://travis@127.0.0.1/drupal -y thunder_module_configure_form.install_modules_thunder_demo
     drush en simpletest -y
 
