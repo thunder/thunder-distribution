8cb8c76bb2785b0b0e0f73c752e9a7d3532460bc
Christian Fritsch <chr.fritsch@gmx.net>
2019-06-03 13:43:38 +0200

    Add dev deps

--- scripts/travis/03-build-thunder2.sh
+++ scripts/travis/03-build-thunder2.sh
@@ -5,6 +5,5 @@ composer create-project BurdaMagazinOrg/thunder-project:2.x ${TEST_DIR} --stabil
 
 cd ${TEST_DIR}
 
-composer remove webflo/drupal-core-require-dev --no-update --dev
-composer require "BurdaMagazinOrg/thunder:^8.2.10" "drupal/dropzonejs:^1.0-alpha3" "drupal/liveblog:^1.0-alpha2" "cweagans/composer-patches:^1.6.6" "drupal-composer/drupal-scaffold:^2.5.3" "zaporylie/composer-drupal-optimizations:^1.1.0" --no-progress --no-update
+composer require "BurdaMagazinOrg/thunder:^8.2.10" "webflo/drupal-core-require-dev:^8.4" "drupal/dropzonejs:^1.0-alpha3" "drupal/liveblog:^1.0-alpha2" "cweagans/composer-patches:^1.6.6" "drupal-composer/drupal-scaffold:^2.5.3" "zaporylie/composer-drupal-optimizations:^1.1.0" --no-progress --no-update
 COMPOSER_MEMORY_LIMIT=-1 composer update --no-progress --prefer-lowest
