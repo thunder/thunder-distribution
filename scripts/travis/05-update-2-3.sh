8347182d4c618971ecbdb287c7da707aa6316f16
Christian Fritsch <chr.fritsch@gmx.net>
2019-06-03 17:45:04 +0200

    Foo

--- scripts/travis/05-update-2-3.sh
+++ scripts/travis/05-update-2-3.sh
@@ -10,11 +10,12 @@ cd ${TEST_DIR}
 composer config repositories.thunder path ${THUNDER_DIST_DIR}
 
 composer remove burdamagazinorg/thunder --no-update
-composer require "thunder/thunder-distribution:*" "drupal/media_entity:^2" --no-update
-
+composer require "thunder/thunder-distribution:*" --no-update
 composer remove drupal/dropzonejs --no-update
 
 composer update
+
+composer require "drupal/media_entity:^2.0" "drupal/video_embed_field:^2.0"
 
 cd ${TEST_DIR}/docroot
 drush updb -y
