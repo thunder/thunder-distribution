5d6c7d3b4c62f979e2827eab00cad000d42d1138
Christian Fritsch <chr.fritsch@gmx.net>
2019-06-03 17:25:42 +0200

    Foo

--- scripts/travis/05-update-2-3.sh
+++ scripts/travis/05-update-2-3.sh
@@ -5,13 +5,15 @@ composer update
 
 cd ${TEST_DIR}/docroot
 drush updb -y
-drush entup -y
 
 cd ${TEST_DIR}
 composer config repositories.thunder path ${THUNDER_DIST_DIR}
 
 composer remove burdamagazinorg/thunder --no-update
-composer require thunder/thunder-distribution:* --no-update
+composer require "thunder/thunder-distribution:*" --no-update
+
+composer remove drupal/dropzonejs --no-update
+
 composer update
 
 cd ${TEST_DIR}/docroot
