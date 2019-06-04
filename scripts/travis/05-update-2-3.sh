051b993adb31445a416769ad16bc383be5b278e1
Christian Fritsch <chr.fritsch@gmx.net>
2019-06-04 11:32:23 +0200

    Add patch

--- scripts/travis/05-update-2-3.sh
+++ scripts/travis/05-update-2-3.sh
@@ -15,10 +15,11 @@ composer remove drupal/dropzonejs --no-update
 
 composer update
 
+mv composer.json composer1.json
+cat composer1.json| jq '.extra.patches += {"drupal/video_embed_field":{"Include upgrade path from video_embed_field":"https://www.drupal.org/files/issues/2018-12-11/2997799-22.patch"}}' > composer.json
 COMPOSER_MEMORY_LIMIT=-1 composer require "drupal/media_entity:^2.0" "drupal/video_embed_field:^2.0" "drupal/media_entity_image"
 
 cd ${TEST_DIR}/docroot
 drush cr
-drush updb -y
 drush updb -y
 
