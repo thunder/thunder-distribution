d19503f6dec205d427a83f29336793690c1e42ed
Christian Fritsch <chr.fritsch@gmx.net>
2019-06-06 14:18:59 +0200

    Disable config_selector

--- scripts/travis/05-update-2-3.sh
+++ scripts/travis/05-update-2-3.sh
@@ -17,12 +17,11 @@ composer update
 
 mv composer.json composer1.json
 cat composer1.json| jq '.extra.patches += {"drupal/video_embed_field":{"Include upgrade path from video_embed_field":"https://www.drupal.org/files/issues/2019-06-04/2997799-25.patch"}}' > composer.json
-mv composer.json composer1.json
-cat composer1.json| jq '.extra.patches += {"drupal/media_entity":{"Update path to core media breaks default view- and form-display config":"https://www.drupal.org/files/issues/2019-06-05/2948704-4.patch"}}' > composer.json
 COMPOSER_MEMORY_LIMIT=-1 composer require "drupal/media_entity:^2.0" "drupal/video_embed_field:^2.0" "drupal/media_entity_image"
 
 cd ${TEST_DIR}/docroot
 drush cr
+drush pmu config_selector -y
 drush updb -y
 drush en vem_migrate_oembed -y
 drush vem:migrate_oembed
