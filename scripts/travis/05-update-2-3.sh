ac88eb5b96db5e396b457c682d7559d6627676c3
Christian Fritsch <chr.fritsch@gmx.net>
2019-06-17 14:53:43 +0200

    Add patches

--- scripts/travis/05-update-2-3.sh
+++ scripts/travis/05-update-2-3.sh
@@ -17,6 +17,13 @@ composer update
 
 mv composer.json composer1.json
 cat composer1.json| jq '.extra.patches += {"drupal/video_embed_field":{"Include upgrade path from video_embed_field":"https://www.drupal.org/files/issues/2019-06-04/2997799-25.patch"}}' > composer.json
+
+mv composer.json composer1.json
+cat composer1.json| jq '.extra.patches += {"drupal/media_entity":{"media_entity_update_8201() doesnt delete fields correctly":"https://www.drupal.org/files/issues/2019-06-17/3062219-4_0.patch"}}' > composer.json
+
+mv composer.json composer1.json
+cat composer1.json| jq '.extra.patches += {"drupal/media_entity":{"Update path to core media breaks default view- and form-display config":"https://www.drupal.org/files/issues/2019-06-17/2948704-7.patch"}}' > composer.json
+
 COMPOSER_MEMORY_LIMIT=-1 composer require "drupal/media_entity:^2.0" "drupal/video_embed_field:^2.0" "drupal/media_entity_image"
 
 cd ${TEST_DIR}/docroot
