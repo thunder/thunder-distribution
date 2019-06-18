9cd712ef664041f98a24e5f874a8865b18f96a4c
Christian Fritsch <chr.fritsch@gmx.net>
2019-06-18 13:39:02 +0200

    Try sponge

--- scripts/travis/05-update-2-3.sh
+++ scripts/travis/05-update-2-3.sh
@@ -14,13 +14,9 @@ composer require "thunder/thunder-distribution:*" --no-update
 
 composer update
 
-mv composer.json composer1.json
-cat composer1.json| jq '.extra.patches += {"drupal/video_embed_field":{"Include upgrade path from video_embed_field":"https://www.drupal.org/files/issues/2019-06-04/2997799-25.patch"}}' > composer.json
+jq '.extra.patches += {"drupal/video_embed_field":{"Include upgrade path from video_embed_field":"https://www.drupal.org/files/issues/2019-06-04/2997799-25.patch"}, "drupal/media_entity":{"media_entity_update_8201() doesnt delete fields correctly":"https://www.drupal.org/files/issues/2019-06-17/3062219-4_0.patch"}}' composer.json | sponge composer.json
 
-mv composer.json composer1.json
-cat composer1.json| jq '.extra.patches += {"drupal/media_entity":{"media_entity_update_8201() doesnt delete fields correctly":"https://www.drupal.org/files/issues/2019-06-17/3062219-4_0.patch"}}' > composer.json
-
-COMPOSER_MEMORY_LIMIT=-1 composer require "drupal/media_entity:2.x-dev" "drupal/video_embed_field:^2.0" "drupal/media_entity_image" "drupal/riddle_marketplace:^3.0-beta2"
+composer require "drupal/media_entity:2.x-dev" "drupal/video_embed_field:^2.0" "drupal/media_entity_image" "drupal/riddle_marketplace:^3.0-beta2"
 
 cd ${TEST_DIR}/docroot
 drush cr
