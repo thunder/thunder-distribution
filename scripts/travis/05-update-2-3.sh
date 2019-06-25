31d74ab806d9bc0cad7429e1a7e0034ffbce789b
Christian Fritsch <chr.fritsch@gmx.net>
2019-06-25 10:48:36 +0200

    [TEST_UPGRADE] Test media_entity patch

--- scripts/travis/05-update-2-3.sh
+++ scripts/travis/05-update-2-3.sh
@@ -14,7 +14,7 @@ composer require "thunder/thunder-distribution:*" --no-update
 
 composer update
 
-jq '.extra.patches += {"drupal/video_embed_field":{"Include upgrade path from video_embed_field":"https://www.drupal.org/files/issues/2019-06-04/2997799-25.patch"}}' composer.json > composer1.json
+jq '.extra.patches += {"drupal/video_embed_field":{"Include upgrade path from video_embed_field":"https://www.drupal.org/files/issues/2019-06-04/2997799-25.patch"}, "drupal/media_entity":{"Ensure media_entity_update_8200() runs first":"https://www.drupal.org/files/issues/2019-06-24/3063716-2.patch"}}' composer.json > composer1.json
 mv composer1.json composer.json
 
 composer require "drupal/media_entity:^2.0-beta4" "drupal/video_embed_field:^2.0" "drupal/media_entity_image" "drupal/riddle_marketplace:^3.0-beta2"
