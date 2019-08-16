fd4e43bbadda7476b20630efea517cbd87fb4ba5
Daniel Bosen <daniel@bosen.eu>
2019-08-16 07:49:17 +0200

    [TEST_EVERYTHING]

--- scripts/travis/05-update-2-3.sh
+++ scripts/travis/05-update-2-3.sh
@@ -12,6 +12,7 @@ composer config repositories.thunder path ${THUNDER_DIST_DIR}
 composer remove burdamagazinorg/thunder --no-update
 composer require "thunder/thunder-distribution:*" --no-update
 
+composer update || true
 composer update
 
 composer require "drupal/media_entity:^2.0-beta4" "drupal/video_embed_field:^2.2" "drupal/media_entity_image" "drupal/riddle_marketplace:^3.0-beta2"
