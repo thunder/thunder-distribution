030887dc3382b6e3f9b1bfdbb3f4948ec3817934
Volker Killesreiter <killesre@gmail.com>
2019-08-27 11:41:29 +0200

    [TEST_EVERYTHING] Add paragraphs upgrade path

--- scripts/travis/05-update-2-3.sh
+++ scripts/travis/05-update-2-3.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+# Update paragraphs to version 1.3 first.
 cd ${TEST_DIR}
+composer require drupal/paragraphs:1.3
+
+cd ${TEST_DIR}/docroot
+drush updb -y
+
+# Update paragraphs to version required by thunder.
+cd ${TEST_DIR}
+composer remove drupal/paragraphs
 composer update
 
 cd ${TEST_DIR}/docroot
