0914c565d9f7f61fc5efb9f387553fec0c78ab3d
Volker Killesreiter <killesre@gmail.com>
2019-08-28 18:04:21 +0200

    [TEST_EVERYTHING] remove update to paragraphs 1.3 step

--- scripts/travis/05-update-2-3.sh
+++ scripts/travis/05-update-2-3.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
-# Update paragraphs to version 1.3 first.
 cd ${TEST_DIR}
-composer require drupal/paragraphs:1.3 --no-update
-composer update
-
-cd ${TEST_DIR}/docroot
-drush updb -y
-
-# Update paragraphs to version required by thunder.
-cd ${TEST_DIR}
-composer remove drupal/paragraphs --no-update
 composer update
 
 cd ${TEST_DIR}/docroot
