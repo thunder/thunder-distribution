9d7d23723844745b9713fdf21225de8e52afb717
Christian Fritsch <chr.fritsch@gmx.net>
2019-09-24 15:49:18 +0200

    [TEST_EVERYTHING]

--- scripts/travis/06-run-tests.sh
+++ scripts/travis/06-run-tests.sh
@@ -21,7 +21,7 @@ if [[ "${TEST_UPDATE}" != "true" ]]; then
 fi
 
 # execute Drupal tests
-thunderDumpFile=thunder.php phpunit --verbose --debug --configuration core --group Thunder $(pwd)/$(drush eval "echo drupal_get_path('profile', 'thunder');")/tests || exit 1
+thunderDumpFile=thunder.php phpunit --verbose --debug --configuration core --group Thunder ${ADDITIONAL_PHPUNIT_PARAMETERS} $(pwd)/$(drush eval "echo drupal_get_path('profile', 'thunder');")/tests || exit 1
 
 if [[ ${TEST_UPDATE} == "true" ]]; then
   thunderDumpFile=thunder.php phpunit --verbose --debug --configuration core --group ThunderInstaller $(pwd)/$(drush eval "echo drupal_get_path('profile', 'thunder');")/tests || exit 1
