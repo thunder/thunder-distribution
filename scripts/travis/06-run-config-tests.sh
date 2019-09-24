ee9bd3930ea2833d3da387f5e7a8e02ebce9bdc6
Christian Fritsch <chr.fritsch@gmx.net>
2019-09-24 17:31:21 +0200

    [TEST_EVERYTHING]

--- scripts/travis/06-run-config-tests.sh
+++ scripts/travis/06-run-config-tests.sh
@@ -5,6 +5,8 @@ source ${THUNDER_DIST_DIR}/scripts/travis/05-setup-tests.sh
 # Run Drupal tests
 cd ${TEST_DIR}/docroot
 
-PHPUNIT=${TEST_DIR}/bin/phpunit
+PHPUNIT=${TEST_DIR}/vendor/bin/phpunit
+
+phpunit --version
 
 thunderDumpFile=thunder.php php ${PHPUNIT} --verbose --debug --configuration core --group ThunderConfig $(pwd)/$(drush eval "echo drupal_get_path('profile', 'thunder');")/tests || exit 1
