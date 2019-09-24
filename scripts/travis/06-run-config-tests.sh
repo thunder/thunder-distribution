2c2306d12b56dd35c82cd0639ba7001ec3871603
Christian Fritsch <chr.fritsch@gmx.net>
2019-09-24 14:14:50 +0200

    Try phpunit

--- scripts/travis/06-run-config-tests.sh
+++ scripts/travis/06-run-config-tests.sh
@@ -5,4 +5,4 @@ source ${THUNDER_DIST_DIR}/scripts/travis/05-setup-tests.sh
 # Run Drupal tests
 cd ${TEST_DIR}/docroot
 
-thunderDumpFile=thunder.php php ${TEST_DIR}/docroot/core/scripts/run-tests.sh --php `which php` --suppress-deprecations --verbose --color --url http://localhost:8080 ThunderConfig
+thunderDumpFile=thunder.php phpunit --verbose --debug --configuration ${TEST_DIR}/docroot/core --group ThunderConfig ${THUNDER_DIST_DIR} || exit 1
