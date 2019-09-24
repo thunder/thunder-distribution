70a299eb8b36e1ddcd01bb09a0acb87bd1bd9dff
Christian Fritsch <chr.fritsch@gmx.net>
2019-09-24 14:25:08 +0200

    Add base url

--- scripts/travis/01-setup-environment.sh
+++ scripts/travis/01-setup-environment.sh
@@ -8,6 +8,7 @@ export TEST_DIR=`echo ${THUNDER_DIST_DIR}"/../test-dir"`
 export PATH="$TEST_DIR/bin:$HOME/.composer/vendor/bin:$PATH"
 export MINK_DRIVER_ARGS_WEBDRIVER='["chrome", null, "http://localhost:4444/wd/hub"]'
 export COMPOSER_MEMORY_LIMIT=-1
+export SIMPLETEST_BASE_URL=http://localhost:8080
 
 # base path for update tests
 export UPDATE_BASE_PATH=${TEST_DIR}-update-base
