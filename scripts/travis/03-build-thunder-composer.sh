81f1f7e0dd788eb1290fd428b58bb31d532785c3
Christian Fritsch <chr.fritsch@gmx.net>
2019-12-23 10:53:58 +0100

    Add rules module

--- scripts/travis/03-build-thunder-composer.sh
+++ scripts/travis/03-build-thunder-composer.sh
@@ -19,7 +19,7 @@ if [[ ${TEST_UPDATE} == "true" ]]; then
 fi
 
 composer config repositories.thunder path ${THUNDER_DIST_DIR}
-composer require "thunder/thunder-distribution:*" "thunder/thunder_testing_demo:3.x-dev" "phpunit/phpunit:^6.5" "composer/composer:^1.9.0" "mglaman/phpstan-drupal:~0.12.0" "phpstan/phpstan-deprecation-rules:~0.12.0" "valiton/harbourmaster:~8.1" --no-progress ${ADDITIONAL_COMPOSER_PARAMETERS}
+composer require "thunder/thunder-distribution:*" "thunder/thunder_testing_demo:3.x-dev" "phpunit/phpunit:^6.5" "composer/composer:^1.9.0" "mglaman/phpstan-drupal:~0.12.0" "phpstan/phpstan-deprecation-rules:~0.12.0" "valiton/harbourmaster:~8.1" "drupal/rules:dev-8.x-3.x" --no-progress ${ADDITIONAL_COMPOSER_PARAMETERS}
 
  # Get custom branch of Thunder Admin theme
 rm -rf ${TEST_DIR}/docroot/themes/contrib/thunder_admin
