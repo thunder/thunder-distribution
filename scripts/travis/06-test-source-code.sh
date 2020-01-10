57b74ef7d275fc6e1b074a930ab7ed166c75a709
Christian Fritsch <chr.fritsch@gmx.net>
2020-01-10 09:44:00 +0100

    Run tests

--- scripts/travis/06-test-source-code.sh
+++ scripts/travis/06-test-source-code.sh
@@ -4,4 +4,4 @@
 cd ${TEST_DIR}
 cp ${THUNDER_DIST_DIR}/phpstan.neon.dist phpstan.neon
 phpstan analyse --memory-limit 1200M ${TEST_DIR}/docroot/modules/contrib -q || true
-phpstan analyse --memory-limit 1200M ${TEST_DIR}/docroot/modules/contrib
+phpstan analyse --memory-limit 1200M ${TEST_DIR}/docroot/modules/contrib || true
