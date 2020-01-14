0e52978dc1363dd1ea43b5dfd0aafbef3320d56e
Christian Fritsch <chr.fritsch@gmx.net>
2020-01-14 13:03:48 +0100

    Use core 8.9

--- scripts/travis/06-run-config-tests.sh
+++ scripts/travis/06-run-config-tests.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+source ${THUNDER_DIST_DIR}/scripts/travis/05-setup-tests.sh
+
 # Run Drupal tests
 cd ${TEST_DIR}/docroot
 
-phpunit --verbose --debug --configuration core --group ThunderConfig ${ADDITIONAL_PHPUNIT_PARAMETERS} $(pwd)/$(drush eval "echo drupal_get_path('profile', 'thunder');")/tests || exit 1
+thunderDumpFile=thunder.php phpunit --verbose --debug --configuration core --group ThunderConfig ${ADDITIONAL_PHPUNIT_PARAMETERS} $(pwd)/$(drush eval "echo drupal_get_path('profile', 'thunder');")/tests || exit 1
