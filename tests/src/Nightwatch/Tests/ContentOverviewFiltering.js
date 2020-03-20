6760a47e5e678058a6371ba90451bfd3c0c45528
Volker Killesreiter <killesre@gmail.com>
2020-03-20 10:30:22 +0100

    align test to sapi version

--- tests/src/Nightwatch/Tests/ContentOverviewFiltering.js
+++ tests/src/Nightwatch/Tests/ContentOverviewFiltering.js
@@ -24,35 +24,25 @@ module.exports = {
       .performance.startMark("full task")
       .performance.startMark("login")
       .drupalLogin({ name: "test-admin", password: "test-admin" })
-      // end "login"
+      // End "login".
       .performance.endMark()
       .performance.startMark("Open content overview page")
       .drupalRelativeURL("/admin/content_bundle_0")
       .useXpath()
-      .moveToElement(
-        '//*[@id="block-thunder-admin-content"]/div/div/nav/ul/li[1]',
-        10,
-        10
-      )
       .waitForElementPresent(
-        '//*[@id="views-form-content-bundle-0-page-1"]/table[2]/tbody/tr[1]'
+        '//*[@id="block-thunder-admin-content"]/div/div/nav/ul/li[1]/a'
       )
-      // end "Open content overview page"
+      // End "Open content overview page".
       .performance.endMark()
       .performance.startMark("Filter by type")
       .setValue('//*[@id="edit-type"]', "bundle_6")
       .click('//*[@id="edit-submit-content-bundle-0"]')
-      .pause(10000)
-      .moveToElement(
-        '//*[@id="block-thunder-admin-content"]/div/div/nav/ul/li[1]',
-        10,
-        10
-      )
       .waitForElementPresent(
-        '//*[@id="views-form-content-bundle-0-page-1"]/table[2]/tbody/tr[1]'
+        '//*[@id="block-thunder-admin-content"]/div/div[contains(@class, "view-content-bundle-0")]/nav[@class="pager"]/ul/li[1]/a[contains(@href, "bundle_6")]'
       )
-      // end "Filter by type"
+      // End "Filter by type".
       .performance.endMark()
+      // End full task.
       .performance.endMark();
 
     // End measurement transaction for whole test.
