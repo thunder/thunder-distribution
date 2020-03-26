a0ea47c22057d846fa3d34a76f3fd9cf7261f673
Bosen Daniel <daniel.bosen@burda.com>
2020-03-26 11:13:41 +0100

    Remove ief test

--- tests/src/FunctionalJavascript/Integration/InlineEntityFormTest.php
+++ tests/src/FunctionalJavascript/Integration/InlineEntityFormTest.php
@@ -45,32 +45,4 @@ class InlineEntityFormTest extends ThunderJavascriptTestBase {
       ->pageTextContains('New gallery name before collapse');
   }
 
-  /**
-   * Test saving collapsed video paragraph.
-   *
-   * Test saving changes in inline entity form using the
-   * inline_entity_form_simple_plus widget inside video paragraph when the
-   * paragraph form is collapsed.
-   *
-   * Demo Article (node Id: 7) is used for testing.
-   */
-  public function testVideoCollapse() {
-
-    // Test saving inline entity form when collapsing paragraph form.
-    $this->drupalGet("node/7/edit");
-    $page = $this->getSession()->getPage();
-
-    // Edit video paragraph.
-    $this->clickButtonCssSelector($page, '[data-drupal-selector="field-paragraphs-2-edit-2"]');
-    $this->setFieldValue($page, 'field_paragraphs[2][subform][field_video][current][items][0][display][field_copyright][0][value]', 'New copyright before collapse');
-
-    // Collapse parargraph form.
-    $this->clickButtonCssSelector($page, '[name="field_paragraphs_2_collapse"]');
-    $this->clickSave();
-
-    // Re-open edit form, value has changed.
-    $this->drupalGet("node/7/edit");
-    $this->assertSession()->pageTextContains('New copyright before collapse');
-  }
-
 }
