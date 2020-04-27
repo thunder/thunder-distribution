0cba65312e8565cf475bb1714d863437fb3fc162
Christian Fritsch <chr.fritsch@gmx.net>
2020-04-27 12:11:18 +0200

    [TEST_EVERYTHING]

--- scripts/travis/05-update-2-3.sh
+++ scripts/travis/05-update-2-3.sh
@@ -30,7 +30,7 @@ composer update
 composer require "thunder/thunder_testing_demo:3.x-dev" "drupal/media_entity:^2.0-beta4" "drupal/video_embed_field:^2.2" "drupal/media_entity_image" "drupal/riddle_marketplace:^3.0-beta2"
 
 cd ${TEST_DIR}/docroot
-drush cr
+drush sqlq "TRUNCATE cache_container;"
 drush updb -y
 drush en vem_migrate_oembed -y
 drush vem:migrate_oembed
