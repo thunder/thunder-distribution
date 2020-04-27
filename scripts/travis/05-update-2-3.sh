c112086dce39f90d509326fa61639851e2ccf3be
Christian Fritsch <chr.fritsch@gmx.net>
2020-04-27 11:53:38 +0200

    [TEST_EVERYTHING]

--- scripts/travis/05-update-2-3.sh
+++ scripts/travis/05-update-2-3.sh
@@ -17,7 +17,6 @@ composer require burdamagazinorg/thunder:~8.2.51 --no-update
 composer update
 
 cd ${TEST_DIR}/docroot
-drush cr
 drush updb -y
 
 cd ${TEST_DIR}
@@ -31,6 +30,7 @@ composer update
 composer require "thunder/thunder_testing_demo:3.x-dev" "drupal/media_entity:^2.0-beta4" "drupal/video_embed_field:^2.2" "drupal/media_entity_image" "drupal/riddle_marketplace:^3.0-beta2"
 
 cd ${TEST_DIR}/docroot
+drush cr
 drush updb -y
 drush en vem_migrate_oembed -y
 drush vem:migrate_oembed
