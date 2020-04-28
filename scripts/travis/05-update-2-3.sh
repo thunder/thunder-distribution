6b73d121b4d1341d2929770136050ef8aa3f2e95
Christian Fritsch <chr.fritsch@gmx.net>
2020-04-28 10:18:32 +0200

    [TEST_EVERYTHING]

--- scripts/travis/05-update-2-3.sh
+++ scripts/travis/05-update-2-3.sh
@@ -29,6 +29,10 @@ composer update
 
 composer require "thunder/thunder_testing_demo:3.x-dev" "drupal/media_entity:^2.0-beta4" "drupal/video_embed_field:^2.2" "drupal/media_entity_image" "drupal/riddle_marketplace:^3.0-beta2"
 
+cd ${TEST_DIR}/docroot/modules/contrib/media_entity
+wget https://www.drupal.org/files/issues/2020-04-28/3131523.patch
+patch -p1 < 3131523.patch
+
 cd ${TEST_DIR}/docroot
 drush updb -y
 drush en vem_migrate_oembed -y
