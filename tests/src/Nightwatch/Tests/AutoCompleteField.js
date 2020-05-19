1e0120dd6ea6c62d196d175254c5e47e8cc66132
Daniel Bosen <daniel.bosen@burda.com>
2020-05-19 11:06:31 +0200

    Update AutoCompleteField.js

--- tests/src/Nightwatch/Tests/AutoCompleteField.js
+++ tests/src/Nightwatch/Tests/AutoCompleteField.js
@@ -8,7 +8,7 @@ const apm = require(`${process.cwd()}/node_modules/elastic-apm-node`);
 const thunderUtils = require("../utils");
 
 module.exports = {
-  "@tags": ["Thunder_disabled", "Thunder_Base_Set_disabled"],
+  "@tags": ["Thunder", "Thunder_Base_Set"],
   before(browser, done) {
     browser.apm = apm;
 
