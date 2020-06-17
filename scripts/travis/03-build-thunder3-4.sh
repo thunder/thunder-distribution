8cbf572a94becf9c9ba28e2bd1efa985c5bb7ab5
Christian Fritsch <chr.fritsch@gmx.net>
2020-06-17 15:09:57 +0200

    fix: add working dir

--- scripts/travis/03-build-thunder3-4.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd ${THUNDER_DIST_DIR}
-composer create-project thunder/thunder-project:3.x ${TEST_DIR} --stability dev --no-interaction --no-install
-
-cd ${TEST_DIR}
-
-cp ${THUNDER_DIST_DIR}/tests/fixtures/thunder3-4.composer.lock composer.lock
-composer install
