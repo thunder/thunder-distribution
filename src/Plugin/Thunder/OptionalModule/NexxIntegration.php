3df1bca313dc571865c85c1b0241828cf3e3a2ed
Christian Fritsch <chr.fritsch@gmx.net>
2020-06-25 09:09:05 +0200

    Remove riddle integration

--- src/Plugin/Thunder/OptionalModule/NexxIntegration.php
+++ src/Plugin/Thunder/OptionalModule/NexxIntegration.php
@@ -5,7 +5,7 @@ namespace Drupal\thunder\Plugin\Thunder\OptionalModule;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Riddle integration.
+ * Nexx integration.
  *
  * @ThunderOptionalModule(
  *   id = "nexx_integration",
