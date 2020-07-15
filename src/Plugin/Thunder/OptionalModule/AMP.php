1efcf968b7fa6232adc5eb1a6041ed104faac348
Christian Fritsch <chr.fritsch@gmx.net>
2020-07-15 15:48:07 +0200

    fix: amp and riddle

--- src/Plugin/Thunder/OptionalModule/AMP.php
+++ src/Plugin/Thunder/OptionalModule/AMP.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\thunder\Plugin\Thunder\OptionalModule;
 
-use Drupal\Core\Form\FormStateInterface;
-
 /**
  * AMP.
  *
@@ -11,33 +9,7 @@ use Drupal\Core\Form\FormStateInterface;
  *   id = "thunder_amp",
  *   label = @Translation("AMP"),
  *   description = @Translation("The Google AMP project strives for better performance, especially on mobile devices."),
- *   type = "theme",
+ *   themes = {"thunder_amp"},
  * )
  */
-class AMP extends AbstractOptionalModule {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-
-    $form = parent::buildForm($form, $form_state);
-
-    if (!class_exists('\Lullabot\AMP\AMP')) {
-      $form['thunder_amp']['library_info'] = [
-        '#type' => 'item',
-        '#description' => $this->t("ATTENTION: Due to licensing issues,
-        you have to download the needed library manually, before activating this module.
-        With composer installed execute the command `composer require lullabot/amp`
-        in the docroot of your installation."),
-      ];
-    }
-    else {
-      unset($form['thunder_amp']);
-    }
-
-    return $form;
-
-  }
-
-}
+class AMP extends AbstractOptionalModule {}
