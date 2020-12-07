ee0e8369ff0a5ce214e3184dda10d757265740d0
Volker Killesreiter <killesre@gmail.com>
2020-12-07 17:15:00 +0100

    add tests

---
+++ tests/src/Functional/BreadcrumbTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\Tests\thunder\Functional;
+
+use Drupal\block\Entity\Block;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Drupal\node\Entity\NodeType;
+use Drupal\Tests\BrowserTestBase;
+use Drupal\Tests\thunder\Functional\ThunderTestBase;
+use Drupal\user\RoleInterface;
+use PHPUnit\Framework\ExpectationFailedException;
+use Drupal\Tests\system\Functional\Menu\AssertBreadcrumbTrait;
+/**
+ * Tests breadcrumbs functionality.
+ *
+ * @group Thunder
+ */
+class BreadcrumbTest extends ThunderTestBase {
+
+
+  use AssertBreadcrumbTrait;
+
+  protected static $modules = [
+    'thunder_testing_demo',
+  ];
+
+  /**
+   * Tests breadcrumbs on node and administrative paths.
+   */
+  public function testBreadCrumbs() {
+
+    $home = [Url::fromRoute('<front>')->toString() => 'Home'];
+    $overview = [Url::fromRoute('system.admin_content')->toString() => 'Overview'];
+    $node_add = [Url::fromRoute('node.add_page')->toString() => 'Add content'];
+
+    // Page content.
+    $this->assertBreadcrumb('node/3', $home + ['/node' => 'Node']);
+    // Article content.
+    $this->assertBreadcrumb('node/8', $home + ['/events' => 'Events']);
+
+    $this->logWithRole('administrator');
+
+    $this->assertBreadcrumb('node/add', $home + $overview);
+    $this->assertBreadcrumb('node/add/article', $home + $overview + $node_add);
+    $this->assertBreadcrumb('node/add/page', $home + $overview + $node_add);
+
+    // Page content.
+    $this->assertBreadcrumb('node/3/edit', $home + $overview);
+    // Article content.
+    $this->assertBreadcrumb('node/8/edit', $home + $overview);
+  }
+}
