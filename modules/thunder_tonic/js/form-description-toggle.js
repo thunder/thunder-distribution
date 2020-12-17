f379f4c77807f9f6d6cd543abbf0c893be3c0799
Christian Fritsch <chr.fritsch@gmx.net>
2020-12-17 09:15:01 +0100

    fix: coding styles

--- modules/thunder_tonic/js/form-description-toggle.js
+++ modules/thunder_tonic/js/form-description-toggle.js
@@ -1,23 +1,27 @@
-/* eslint-disable no-bitwise, no-nested-ternary, no-mutable-exports, comma-dangle, strict */
-
-'use strict';
-
 ((Drupal) => {
   Drupal.behaviors.formDescriptionToggle = {
     attach: function attach(context) {
-      context.querySelectorAll('.help-icon__description-toggle').forEach((elem) => {
-        if (elem.dataset.formDescriptionToggleAttached) {
-          return;
-        }
-        elem.dataset.formDescriptionToggleAttached = true;
+      context
+        .querySelectorAll('.help-icon__description-toggle')
+        .forEach((elem) => {
+          if (elem.dataset.formDescriptionToggleAttached) {
+            return;
+          }
+          elem.dataset.formDescriptionToggleAttached = true;
 
-        elem.addEventListener('click', (event) => {
-          event.preventDefault();
-          event.currentTarget.closest('.help-icon__description-container').querySelectorAll('.form-item__description, .claro-details__description').forEach((elem) => {
-            elem.classList.toggle('visually-hidden');
+          elem.addEventListener('click', (event) => {
+            const toggleFunction = (description) => {
+              description.classList.toggle('visually-hidden');
+            };
+            event.preventDefault();
+            event.currentTarget
+              .closest('.help-icon__description-container')
+              .querySelectorAll(
+                '.form-item__description, .claro-details__description',
+              )
+              .forEach(toggleFunction);
           });
         });
-      });
-    }
+    },
   };
 })(Drupal);
