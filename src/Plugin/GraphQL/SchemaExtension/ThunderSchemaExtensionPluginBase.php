abccc22acbdd216f1efd46c0948ca8f959cc6f01
Christian Fritsch <chr.fritsch@gmx.net>
2021-05-04 10:13:23 +0200

    Entity links

--- src/Plugin/GraphQL/SchemaExtension/ThunderSchemaExtensionPluginBase.php
+++ src/Plugin/GraphQL/SchemaExtension/ThunderSchemaExtensionPluginBase.php
@@ -138,6 +138,10 @@ abstract class ThunderSchemaExtensionPluginBase extends SdlSchemaExtensionPlugin
         ->map('value', $this->builder->fromParent())
     );
 
+    $this->addFieldResolverIfNotExists($type, 'entityLinks',
+      $this->builder->produce('entity_links')
+        ->map('entity', $this->builder->fromParent())
+    );
   }
 
   /**
