0b1ecb8ba2d4dcf0ae3f813233778303c025c582
Christian Fritsch <chr.fritsch@gmx.net>
2021-05-10 11:18:29 +0200

    Empty commit

--- tests/src/FunctionalJavascript/ArticleCreationTest.php
+++ tests/src/FunctionalJavascript/ArticleCreationTest.php
@@ -68,7 +68,8 @@ class ArticleCreationTest extends ThunderJavascriptTestBase {
     $this->addLinkParagraph(static::$paragraphsField, 'Link to Thunder', 'http://www.thunder.org');
 
     // Add Video paragraph at the beginning.
-    $this->addVideoParagraph(static::$paragraphsField, ['media:20'], 0);
+    $video = $this->getMediaByName('Youtube');
+    $this->addVideoParagraph(static::$paragraphsField, ['media:' . $video->id()], 0);
 
     // Add Pinterest Paragraph.
     $this->addSocialParagraph(static::$paragraphsField, 'https://www.pinterest.de/pin/478085316687452268/', 'pinterest');
