9463baba16b7fd70546ef66d527b828ef625f491
Christian Fritsch <chr.fritsch@gmx.net>
2021-05-27 15:04:44 +0200

    Use access unpublished route load (#33)

--- src/Plugin/GraphQL/SchemaExtension/ThunderRoutingSchemaExtension.php
+++ src/Plugin/GraphQL/SchemaExtension/ThunderRoutingSchemaExtension.php
@@ -3,6 +3,8 @@
 namespace Drupal\thunder_gqls\Plugin\GraphQL\SchemaExtension;
 
 use Drupal\graphql\GraphQL\ResolverRegistryInterface;
+use Drupal\graphql\Plugin\DataProducerPluginManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * The routing schema extension.
@@ -17,18 +19,54 @@ use Drupal\graphql\GraphQL\ResolverRegistryInterface;
 class ThunderRoutingSchemaExtension extends ThunderSchemaExtensionPluginBase {
 
   /**
+   * The data producer plugin manager.
+   *
+   * @var \Drupal\graphql\Plugin\DataProducerPluginManager
+   */
+  protected $dataProducerManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $extension = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $extension->setDataProducerManager($container->get('plugin.manager.graphql.data_producer'));
+    return $extension;
+  }
+
+  /**
+   * Set the plugin manager.
+   *
+   * @param \Drupal\graphql\Plugin\DataProducerPluginManager $pluginManager
+   *   The data producer plugin manager.
+   */
+  protected function setDataProducerManager(DataProducerPluginManager $pluginManager) {
+    $this->dataProducerManager = $pluginManager;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function registerResolvers(ResolverRegistryInterface $registry) {
     parent::registerResolvers($registry);
 
-    $this->addFieldResolverIfNotExists('Query', 'page', $this->builder->compose(
-      $this->builder->produce('route_load')
-        ->map('path', $this->builder->fromArgument('path')),
-      $this->builder->produce('route_entity')
-        ->map('url', $this->builder->fromParent())
-
-    ));
+    if ($this->dataProducerManager->hasDefinition('access_unpublished_route_load')) {
+      $this->addFieldResolverIfNotExists('Query', 'page', $this->builder->compose(
+        $this->builder->produce('access_unpublished_route_load')
+          ->map('path', $this->builder->fromArgument('path'))
+          ->map('token', $this->builder->fromArgument('auHash')),
+        $this->builder->produce('route_entity')
+          ->map('url', $this->builder->fromParent())
+      ));
+    }
+    else {
+      $this->addFieldResolverIfNotExists('Query', 'page', $this->builder->compose(
+        $this->builder->produce('route_load')
+          ->map('path', $this->builder->fromArgument('path')),
+        $this->builder->produce('route_entity')
+          ->map('url', $this->builder->fromParent())
+      ));
+    }
   }
 
 }
--- tests/src/Functional/SchemaTest.php
+++ tests/src/Functional/SchemaTest.php
@@ -2,6 +2,10 @@
 
 namespace Drupal\Tests\thunder_gqls\Functional;
 
+use Drupal\access_unpublished\Entity\AccessToken;
+use Drupal\Component\Serialization\Json;
+use Drupal\node\Entity\Node;
+
 /**
  * Test the schema.
  *
@@ -44,6 +48,56 @@ class SchemaTest extends ThunderGqlsTestBase {
         'breadcrumb',
       ],
     ];
+  }
+
+  /**
+   * Tests the article schema.
+   *
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
+  public function testUnpublishedAccess() {
+
+    $node = Node::create([
+      'title' => 'Test node',
+      'type' => 'article',
+      'status' => Node::NOT_PUBLISHED,
+    ]);
+    $node->save();
+
+    $validToken = AccessToken::create([
+      'entity_type' => 'node',
+      'entity_id' => $node->id(),
+      'value' => 'iAmValid',
+      'expire' => -1,
+    ]);
+    $validToken->save();
+
+    $query = <<<GQL
+      query (\$path: String!) {
+        page(path: \$path) {
+          name
+        }
+      }
+GQL;
+
+    $variables = ['path' => $node->toUrl()->toString()];
+    $response = $this->query($query, Json::encode($variables));
+    $this->assertEquals(200, $response->getStatusCode(), 'Response not 200');
+    $this->assertEmpty(json_decode($response->getBody(), TRUE)['data']['page']);
+
+    $query = <<<GQL
+      query (\$path: String!, \$token: String!) {
+        page(path: \$path, auHash: \$token) {
+          name
+        }
+      }
+GQL;
+
+    $variables = ['path' => $node->toUrl()->toString(), 'token' => 'iAmValid'];
+    $response = $this->query($query, Json::encode($variables));
+    $this->assertEquals(200, $response->getStatusCode(), 'Response not 200');
+
+    $this->assertEqualsCanonicalizing(['name' => 'Test node'], json_decode($response->getBody(), TRUE)['data']['page']);
   }
 
 }
