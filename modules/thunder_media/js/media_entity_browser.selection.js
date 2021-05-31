7e3e85aed104f879d297416b35c46b4ac4f04723
Volker Killesreiter <killesre@gmail.com>
2021-05-31 12:21:21 +0200

    fix eslint

--- modules/thunder_media/js/media_entity_browser.selection.js
+++ modules/thunder_media/js/media_entity_browser.selection.js
@@ -4,30 +4,34 @@
  */
 
 ((Drupal, once) => {
+  function init(element) {
+    element.querySelectorAll('.views-row').forEach((row) => {
+      row.addEventListener('click', (event) => {
+        event.preventDefault();
+        const input = row.querySelector(
+          '.views-field-entity-browser-select input.form-checkbox, .views-field-entity-browser-select input.form-radio',
+        );
+        input.checked = input.checked ? false : 'checked';
+        row.classList[input.checked ? 'add' : 'remove']('checked');
+      });
+    });
+  }
+
   /**
    * Selection of media entities in the entity browser view.
    */
   Drupal.behaviors.mediaEntityBrowserSelection = {
-    attach: function (context, settings) {
+    attach: function attach(context, settings) {
       // Autoselect is handled by entity_browser.view.js.
       if (settings.entity_browser_widget.auto_select) {
         return;
       }
 
-      once('entityBrowserSelection', 'form.entity-browser-form', context).forEach(
-        (element) => {
-          element.querySelectorAll('.views-row').forEach(
-            (row) => {
-              row.addEventListener('click', (event) => {
-                event.preventDefault();
-                const input = row.querySelector('.views-field-entity-browser-select input.form-checkbox, .views-field-entity-browser-select input.form-radio');
-                input.checked = input.checked ? false : 'checked';
-                row.classList[input.checked ? 'add' : 'remove']('checked');
-              });
-            }
-          );
-        }
-      );
+      once(
+        'entityBrowserSelection',
+        'form.entity-browser-form',
+        context,
+      ).forEach(init);
     },
   };
 })(Drupal, once);
