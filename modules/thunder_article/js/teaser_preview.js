e0c3a4eee240be45a5b503f18ea29ab4bfa3e94b
Christian Fritsch <chr.fritsch@gmx.net>
2021-07-15 17:08:44 +0200

    fix: coding style

--- modules/thunder_article/js/teaser_preview.js
+++ modules/thunder_article/js/teaser_preview.js
@@ -1,7 +1,4 @@
-(function (Drupal) {
-
-  'use strict';
-
+((Drupal) => {
   /**
    * Define teaser preview template.
    *
@@ -11,45 +8,47 @@
    * @return {string}
    *   Returns markup string for teaser preview.
    */
-  Drupal.theme.thunderArticleTeaserPreview = function (config) {
-    return '' +
-      '<img src="' + config.image + '"/>' +
-      '<h1>' + config.title + '</h1>' +
-      '<p>' + config.text + '</p>';
+  Drupal.theme.thunderArticleTeaserPreview = (config) => {
+    return (
+      `<img src="${config.image}"/>` +
+      `<h1>${config.title}</h1>` +
+      `<p>${config.text}</p>`
+    );
   };
 
-
   Drupal.behaviors.teaserPreview = {
-    attach: function (context, settings) {
-
-      var title = document.querySelector('[data-drupal-selector="edit-field-seo-title-0-value"]')
-      var text = document.querySelector('[data-drupal-selector="edit-field-teaser-text-0-value"]')
-      console.log(text)
+    attach() {
+      const title = document.querySelector(
+        '[data-drupal-selector="edit-field-seo-title-0-value"]',
+      );
+      const text = document.querySelector(
+        '[data-drupal-selector="edit-field-teaser-text-0-value"]',
+      );
 
       title.addEventListener('input', (e) => {
-        var titleField = document.querySelector('article.teaser-preview h1')
-        titleField.textContent = e.target.value
-      })
+        const titleField = document.querySelector('article.teaser-preview h1');
+        titleField.textContent = e.target.value;
+      });
 
       text.addEventListener('input', (e) => {
-        var textField = document.querySelector('article.teaser-preview p')
+        const textField = document.querySelector('article.teaser-preview p');
         textField.innerHTML = e.target.value.replace(/(?:\r\n|\r|\n)/g, '<br>');
-      })
+      });
 
-      var imageSrc = ''
-      var imageField = document.querySelector('[data-drupal-selector="edit-field-teaser-media-wrapper"] img')
+      let imageSrc = '';
+      const imageField = document.querySelector(
+        '[data-drupal-selector="edit-field-teaser-media-wrapper"] img',
+      );
       if (imageField) {
-        imageSrc = imageField.src
+        imageSrc = imageField.src;
       }
 
-      var container = document.querySelector('article.teaser-preview')
+      const container = document.querySelector('article.teaser-preview');
       container.innerHTML = Drupal.theme('thunderArticleTeaserPreview', {
         image: imageSrc,
         title: title.value,
-        text: text.value.replace(/(?:\r\n|\r|\n)/g, '<br>')
+        text: text.value.replace(/(?:\r\n|\r|\n)/g, '<br>'),
       });
-
-    }
+    },
   };
-
 })(Drupal);
