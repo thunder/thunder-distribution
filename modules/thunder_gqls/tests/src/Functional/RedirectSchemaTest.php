8b1ff23f6ed4a1392b34f40b1f6bf54544f995ce
Daniel Bosen <daniel.bosen@burda.com>
2021-11-29 17:24:03 +0100

    Test redirects differently

---
+++ modules/thunder_gqls/tests/src/Functional/RedirectSchemaTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\Tests\thunder_gqls\Functional;
+
+use Drupal\Component\Serialization\Json;
+
+/**
+ * Test the redirect endpoint.
+ *
+ * @group Thunder
+ */
+class RedirectSchemaTest extends ThunderGqlsTestBase {
+
+  /**
+   * Tests the jsonld extension.
+   *
+   * @dataProvider redirectTestCases
+   *
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
+  public function testRedirect(array $variables, array $expectedResponse) {
+    $schema = "redirect";
+    $query = $this->getQueryFromFile($schema);
+
+    $response = $this->query($query, Json::encode($variables));
+    $this->assertEquals(200, $response->getStatusCode(), 'Response not 200');
+
+    $responseData = Json::decode($response->getBody())['data'];
+
+    $this->assertEqualsCanonicalizing($expectedResponse, $responseData);
+  }
+
+  /**
+   * A data provider for testRedirect.
+   */
+  public function redirectTestCases() {
+    return [
+      'Basic redirect' => [
+        [
+          'path' => '/former-url',
+        ],
+        [
+          'redirect' => [
+            'url' => 'https://www.google.com',
+            'status' => '301',
+          ],
+        ],
+      ],
+    ];
+  }
+
+}
