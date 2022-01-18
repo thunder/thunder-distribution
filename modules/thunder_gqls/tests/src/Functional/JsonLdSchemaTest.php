eec52bcea5101d37a967a41a08e3ae5c649b9d22
Christian Fritsch <chr.fritsch@gmx.net>
2022-01-18 17:14:52 +0100

    Issue #3259163 by chr.fritsch, daniel.bosen: Add structured data to Thunder article by a JSON-LD schema

--- modules/thunder_gqls/tests/src/Functional/JsonLdSchemaTest.php
+++ modules/thunder_gqls/tests/src/Functional/JsonLdSchemaTest.php
@@ -10,26 +10,14 @@ namespace Drupal\Tests\thunder_gqls\Functional;
 class JsonLdSchemaTest extends ThunderGqlsTestBase {
 
   /**
-   * {@inheritdoc}
-   */
-  protected static $modules = [
-    'schema_article',
-  ];
-
-  /**
    * Tests the jsonld extension.
    *
    * @throws \GuzzleHttp\Exception\GuzzleException
    */
   public function testSchema() {
     $tags = $this->config('metatag.metatag_defaults.node__article')->get('tags');
-    $tags['schema_article_type'] = 'Article';
+    unset($tags['schema_article_date_modified'], $tags['schema_article_image'], $tags['schema_article_publisher']);
     $this->config('metatag.metatag_defaults.node__article')->set('tags', $tags)
-      ->save();
-
-    $extensions = $this->config('graphql.graphql_servers.thunder_graphql')->get('schema_configuration.thunder.extensions');
-    $extensions['thunder_jsonld'] = 'thunder_jsonld';
-    $this->config('graphql.graphql_servers.thunder_graphql')->set('schema_configuration.thunder.extensions', $extensions)
       ->save();
 
     $this->drupalLogin($this->graphqlUser);
