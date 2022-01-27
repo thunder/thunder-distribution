157ea989f1e39b3bb6983b4f7d2d8351be4e5bc6
Christian Fritsch <chr.fritsch@gmx.net>
2022-01-27 14:22:28 +0100

    fix: moar tests moved

--- tests/src/Functional/Integration/ContentLockTest.php
+++ tests/src/Functional/Integration/ContentLockTest.php
@@ -1,20 +1,30 @@
 <?php
 
-namespace Drupal\Tests\thunder\FunctionalJavascript\Integration;
+namespace Drupal\Tests\thunder\Functional\Integration;
 
-use Drupal\Tests\thunder\FunctionalJavascript\ThunderJavascriptTestBase;
+use Drupal\Tests\thunder\Functional\ThunderTestBase;
 
 /**
  * Tests integration with the content_lock module.
  *
  * @group Thunder
  */
-class ContentLockTest extends ThunderJavascriptTestBase {
+class ContentLockTest extends ThunderTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'thunder_testing_demo',
+    'thunder_workflow',
+    'thunder_test_mock_request',
+  ];
 
   /**
    * Testing the content lock integration.
    */
   public function testContentLock(): void {
+    $this->logWithRole('editor');
 
     $node = $this->loadNodeByUuid('0bd5c257-2231-450f-b4c2-ab156af7b78d');
     $this->drupalGet($node->toUrl('edit-form'));
@@ -32,7 +42,7 @@ class ContentLockTest extends ThunderJavascriptTestBase {
     $this->drupalLogout();
 
     // Login with other user.
-    $this->logWithRole(static::$defaultUserRole);
+    $this->logWithRole('restricted_editor');
 
     $this->drupalGet($node->toUrl('edit-form'));
     $this->assertSession()->pageTextContains('This content is being edited by the user ' . $loggedInUser . ' and is therefore locked to prevent other users changes.');
