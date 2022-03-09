c18f5316359a1c1cdcd0361c60b23828d55fd543
Daniel Bosen <daniel.bosen@burda.com>
2022-03-09 14:45:53 +0100

    Keep body field.

--- modules/thunder_gqls/src/Plugin/GraphQL/SchemaExtension/ThunderBcLayerBasicPageBodySchemaExtension.php
+++ modules/thunder_gqls/src/Plugin/GraphQL/SchemaExtension/ThunderBcLayerBasicPageBodySchemaExtension.php
@@ -8,13 +8,13 @@ use Drupal\graphql\GraphQL\ResolverRegistryInterface;
  * The menu schema extension.
  *
  * @SchemaExtension(
- *   id = "thunder_bc_layer_basic_page_content",
- *   name = "BC layer for basic page content field.",
- *   description = "BC layer for basic page content field.",
+ *   id = "thunder_bc_layer_basic_page_body",
+ *   name = "BC layer for basic page body field.",
+ *   description = "BC layer for basic page body field. Not needed, if you use the paragraphs field instead of the body field in the basic page content type.",
  *   schema = "thunder"
  * )
  */
-class ThunderBcLayerBasicPageContentSchemaExtension extends ThunderSchemaExtensionPluginBase {
+class ThunderBcLayerBasicPageBodySchemaExtension extends ThunderSchemaExtensionPluginBase {
 
   /**
    * {@inheritdoc}
@@ -22,7 +22,7 @@ class ThunderBcLayerBasicPageContentSchemaExtension extends ThunderSchemaExtensi
   public function registerResolvers(ResolverRegistryInterface $registry): void {
     parent::registerResolvers($registry);
 
-    $this->registry->addFieldResolver('BasicPage', 'content',
+    $this->registry->addFieldResolver('BasicPage', 'body',
       $this->builder->fromPath('entity', 'body.processed')
     );
   }
