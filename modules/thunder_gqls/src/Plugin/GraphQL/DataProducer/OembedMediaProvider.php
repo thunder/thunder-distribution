72a3b3df5fa470ca6d60f23c145a0688ecbb51a9
Daniel Bosen <daniel.bosen@burda.com>
2022-08-24 10:11:06 +0200

    fix cs

--- modules/thunder_gqls/src/Plugin/GraphQL/DataProducer/OembedMediaProvider.php
+++ modules/thunder_gqls/src/Plugin/GraphQL/DataProducer/OembedMediaProvider.php
@@ -2,10 +2,8 @@
 
 namespace Drupal\thunder_gqls\Plugin\GraphQL\DataProducer;
 
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\graphql\Plugin\GraphQL\DataProducer\DataProducerPluginBase;
 use Drupal\media\MediaInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Resolves the provider of an embed media entity.
@@ -24,26 +22,19 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   }
  * )
  */
-class OembedMediaProvider extends DataProducerPluginBase implements ContainerFactoryPluginInterface {
-
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition
-    );
-  }
+class OembedMediaProvider extends DataProducerPluginBase {
 
   /**
    * Resolve the focal point positions.
    *
    * @param \Drupal\media\MediaInterface $media
+   *   The media entity.
    *
    * @return string
    *   The provider.
    */
   public function resolve(MediaInterface $media): string {
-    return strtolower($media->getSource()->getMetadata($media, 'provider_name'))?:'';
+    return strtolower($media->getSource()->getMetadata($media, 'provider_name')) ?: '';
   }
 
 }
