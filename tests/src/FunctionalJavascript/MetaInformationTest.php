eafdadac058e4dbb7568808142a6c670a333a802
Daniel Bosen <daniel.bosen@burda.com>
2022-11-23 09:52:22 +0100

    Use correct sitemap

--- tests/src/FunctionalJavascript/MetaInformationTest.php
+++ tests/src/FunctionalJavascript/MetaInformationTest.php
@@ -365,7 +365,7 @@ class MetaInformationTest extends ThunderJavascriptTestBase {
     $this->sitemapGenerator->saveSetting('xsl', FALSE);
     $this->sitemapGenerator->generateSitemap('backend');
 
-    $this->drupalGet('article/sitemap.xml');
+    $this->drupalGet($contentType . '/sitemap.xml');
 
     $content = $this->getSession()->getPage()->getContent();
     $domElements = $this->getSiteMapDomElements($content, '//sm:loc[contains(text(),"/' . $articleUrl . '")]/parent::sm:url/sm:priority');
