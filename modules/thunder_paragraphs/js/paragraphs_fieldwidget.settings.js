3f870b802efc63fa44da64b449a029becddc830c
Volker Killesreiter <killesre@gmail.com>
2023-02-28 16:53:02 +0100

    test fail

--- modules/thunder_paragraphs/js/paragraphs_fieldwidget.settings.js
+++ modules/thunder_paragraphs/js/paragraphs_fieldwidget.settings.js
@@ -1,58 +1,51 @@
 /**
  * @file
- * Show warnings on paragraphs widget third party settings.
+ * Defines the behavior of the media entity browser view.
  */
-((Drupal, once) => {
-  /**
-   * Theme function displaying a warning.
-   *
-   * @param {object} options.name
-   *   The name of the setting.
-   *
-   * @return {string}
-   *   Returns markup.
-   */
+(($, Drupal) => {
   Drupal.theme.thunderParagraphsFieldWidgetSettingsWarning = (options) => {
     const message = Drupal.t(
       'The !option option is not supported for the Thunder distribution because of potential data loss in combination with the inline_entity_form module. If you want to use it, make sure to remove all inline entity forms from your paragraph types.',
-      { '!option': options.name },
+      { '!option': options.option },
     );
-    return Drupal.theme('message', { text: message }, { type: 'warning' });
+    return `<div class="messages messages--warning js-form-wrapper form-wrapper">${message}</div>`;
   };
 
   /**
    * Display warning message for certain paragraphs field widget settings.
    */
   Drupal.behaviors.thunderParagraphsFieldWidgetSettings = {
-    attach: function (context) {
-      const form = once(
-        'paragraphsFieldWidgetSettings',
+    attach(context) {
+      const $form = $(
         '[data-drupal-selector="edit-fields-field-paragraphs-settings-edit-form"]',
         context,
-      ).shift();
-
-      if (!form) {
-        return;
+      ).once('paragraphsFieldWidgetSettings');
+      if ($form.length) {
+        // Autocollapse
+        $form
+          .find(
+            '[data-drupal-selector="edit-fields-field-paragraphs-settings-edit-form-settings-autocollapse"]',
+          )
+          .parents('.form-item__field-wrapper')
+          .first()
+          .after(
+            Drupal.theme('thunderParagraphsFieldWidgetSettingsWarning', {
+              option: Drupal.t('Autocollapse'),
+            }),
+          );
+        // Collapse / Edit all
+        $form
+          .find(
+            '[data-drupal-selector="edit-fields-field-paragraphs-settings-edit-form-settings-features-collapse-edit-all"]',
+          )
+          .parents('.form-item__field-wrapper')
+          .first()
+          .after(
+            Drupal.theme('thunderParagraphsFieldWidgetSettingsWarning', {
+              option: Drupal.t('Collapse / Edit all'),
+            }),
+          );
       }
-      // Autocollapse
-      let elem = form.querySelector(
-        '[data-drupal-selector="edit-fields-field-paragraphs-settings-edit-form-settings-autocollapse"]',
-      );
-      let message = Drupal.theme(
-        'thunderParagraphsFieldWidgetSettingsWarning',
-        { name: Drupal.t('Autocollapse') },
-      );
-      elem.closest('.form-item').appendChild(message);
-
-      // Collapse / Edit all
-      elem = form.querySelector(
-        '[data-drupal-selector="edit-fields-field-paragraphs-settings-edit-form-settings-features-collapse-edit-all"]',
-      );
-      message = Drupal.theme(
-        'thunderParagraphsFieldWidgetSettingsWarning',
-        { name: Drupal.t('Collapse / Edit all') },
-      );
-      elem.closest('.fieldset-wrapper').appendChild(message);
     },
   };
-})(Drupal, once);
+})(jQuery, Drupal);
