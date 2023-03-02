729f2e32580976b95c0a5a0563643dc1db3ebc9f
Daniel Bosen <daniel.bosen@burda.com>
2023-03-02 10:05:23 +0100

    lint

--- modules/thunder_article/js/article-form.js
+++ modules/thunder_article/js/article-form.js
@@ -1,15 +1,17 @@
-(($, Drupal, once) => {
+((Drupal, once) => {
   Drupal.behaviors.myBehavior = {
     attach: (context) => {
-      once('thunderEditForm', '.region-content form.gin-node-edit-form', context).forEach(
-        (element) => {
-          $(element).on('keydown', ':input:not(textarea)', (event) => {
-            if (event.key === 'Enter') {
-              event.preventDefault();
-            }
-          });
-        }
-      );
-    }
+      once(
+        'thunderEditForm',
+        '.region-content form.gin-node-edit-form',
+        context,
+      ).forEach((element) => {
+        element.addEventListener('keydown', (event) => {
+          if (event.key === 'Enter') {
+            event.preventDefault();
+          }
+        });
+      });
+    },
   };
-})(jQuery, Drupal, once);
+})(Drupal, once);
