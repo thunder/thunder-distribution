5d5a7967eb251642d31d492163ddb5221d598cfe
Volker Killesreiter <killesre@gmail.com>
2023-03-06 16:39:36 +0100

    streamline

--- modules/thunder_article/js/article-form.js
+++ modules/thunder_article/js/article-form.js
@@ -1,14 +1,15 @@
 ((Drupal, once) => {
-  Drupal.behaviors.myBehavior = {
+  Drupal.behaviors.disableImplicitSubmission = {
     attach: (context) => {
       once(
-        'thunderEditForm',
-        '.region-content form.gin-node-edit-form input[type="text"],input[type="date"]',
+        'thunderDisableImplicitSubmission',
+        'form.gin-node-edit-form input[type="text"], form.gin-node-edit-form input[type="date"]',
         context,
       ).forEach((element) => {
         element.addEventListener('keydown', (event) => {
           if (event.key === 'Enter') {
-            //event.preventDefault();
+            event.preventDefault();
+            event.stopPropagation();
           }
         });
       });
