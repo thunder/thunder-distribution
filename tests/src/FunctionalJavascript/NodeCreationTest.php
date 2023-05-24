884e8708f24bec891a4c07d871c7f0de99539249
Daniel Bosen <daniel.bosen@burda.com>
2023-05-24 14:22:11 +0200

    sleep

--- tests/src/FunctionalJavascript/NodeCreationTest.php
+++ tests/src/FunctionalJavascript/NodeCreationTest.php
@@ -100,6 +100,7 @@ class NodeCreationTest extends ThunderJavascriptTestBase {
     $this->assertSession()->pageTextContains('Awesome quote');
 
     // Check that one Instagram widget is on page.
+    sleep(100);
     $numOfElements = $this->getSession()->wait(5000, 'document.querySelectorAll("iframe[src*=\'instagram.com/p/B2huuS8AQVq\']").length');
     $this->assertEquals(1, $numOfElements, "Number of instagrams on page should be one.");
 
