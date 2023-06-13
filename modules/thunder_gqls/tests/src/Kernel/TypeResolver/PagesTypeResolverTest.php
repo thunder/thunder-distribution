a27383245a2a177cb3683d77aba44b5904576220
Daniel Bosen <daniel.bosen@burda.com>
2023-06-13 08:06:54 +0200

    Fex code style issues.

--- modules/thunder_gqls/tests/src/Kernel/TypeResolver/PagesTypeResolverTest.php
+++ modules/thunder_gqls/tests/src/Kernel/TypeResolver/PagesTypeResolverTest.php
@@ -3,9 +3,13 @@
 namespace Drupal\Tests\thunder_gqls\Kernel\TypeResolver;
 
 use Drupal\node\NodeInterface;
+use Drupal\Tests\graphql\Kernel\GraphQLTestBase;
 use Drupal\thunder_gqls\GraphQL\PagesTypeResolver;
 use Drupal\Tests\graphql\Kernel\GraphQLTestBase;
 
+/**
+ * Test the pages type resolver.
+ */
 class PagesTypeResolverTest extends GraphQLTestBase {
 
   /**
@@ -15,19 +19,24 @@ class PagesTypeResolverTest extends GraphQLTestBase {
     'thunder_gqls',
   ];
 
+  /**
+   * {@inheritdoc}
+   */
   public function setUp(): void {
     parent::setUp();
   }
 
-  public function testResolve() {
+  /**
+   * Test the resolve method.
+   */
+  public function testResolve(): void {
     $node = $this->createMock(NodeInterface::class);
     $node->expects($this->exactly(2))
       ->method('bundle')
       ->willReturnOnConsecutiveCalls('article', 'page');
 
-
     $resolver = new PagesTypeResolver(NULL);
     $this->assertEquals('Article', $resolver->__invoke($node));
-    $this->assertEquals('BasicPage', $resolver->__invoke($node));
   }
+
 }
