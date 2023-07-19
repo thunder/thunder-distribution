f3eeaf742c502d2e88661b2b88f42d45e435a58b
Christian Fritsch <christian.fritsch@burda.com>
2023-07-19 15:25:32 +0200

    Check

--- modules/thunder_text_export/js/copyToClipboard.js
+++ modules/thunder_text_export/js/copyToClipboard.js
@@ -8,6 +8,11 @@
    *  The ajax response.
    */
   Drupal.AjaxCommands.prototype.copyToClipboard = (ajax, response) => {
-    navigator.clipboard.writeText(response.message);
+    if (window.isSecureContext && navigator.clipboard) {
+      navigator.clipboard.writeText(response.message);
+    }
+    else {
+      alert('Copied to clipboard requires a secure context (HTTPS served website) and navigator.clipboard support.')
+    }
   };
 })(Drupal);
