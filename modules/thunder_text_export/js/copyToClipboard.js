74ef7526145580f240ce7528e3822af975d81f32
Christian Fritsch <christian.fritsch@burda.com>
2023-07-21 14:25:39 +0200

    ui dinge

--- modules/thunder_text_export/js/copyToClipboard.js
+++ modules/thunder_text_export/js/copyToClipboard.js
@@ -10,6 +10,20 @@
   Drupal.AjaxCommands.prototype.copyToClipboard = (ajax, response) => {
     if (window.isSecureContext && navigator.clipboard) {
       navigator.clipboard.writeText(response.message);
+      // Change a button value to "Copied" for 2 seconds.
+      const button = document.getElementById('edit-copytoclipboard--2--gin-edit-form');
+
+      // Get button width and set it to the button.
+      const buttonWidth = button.offsetWidth;
+      button.style.width = `${buttonWidth}px`;
+
+      button.value = Drupal.t('Copied ✓');
+      button.disabled = true;
+      setTimeout(() => {
+        button.value = Drupal.t('Text Export');
+        button.disabled = false;
+      }, 1500);
+
     } else {
       // eslint-disable-next-line no-alert
       alert(
