c716670f87357cb93ed273cbd9cb2adff4c703cf
Daniel Bosen <daniel.bosen@burda.com>
2023-10-24 14:47:33 +0200

    restrict to fieldname

--- tests/src/FunctionalJavascript/MediaGalleryModifyTest.php
+++ tests/src/FunctionalJavascript/MediaGalleryModifyTest.php
@@ -67,11 +67,11 @@ JS;
 
     $this->clickSave();
 
-    $this->clickCssSelector('div.slick--initialized button.slick-next');
+    $this->clickCssSelector('div.slick--initialized.slick--field-media-images button.slick-next');
 
     // Check that, 2nd image is file: 26357237683_0891e46ba5_k.jpg.
     $fileNamePosition = $this->getSession()
-      ->evaluateScript('jQuery(\'div.slick--initialized div.slick-slide:not(.slick-cloned):nth(1) img\').attr(\'src\').indexOf("26357237683_0891e46ba5_k.jpg")');
+      ->evaluateScript('jQuery(\'div.slick--initialized.slick--field-media-images div.slick-slide:not(.slick-cloned):nth(1) img\').attr(\'src\').indexOf("26357237683_0891e46ba5_k.jpg")');
     $this->assertNotEquals(-1, $fileNamePosition, 'For 2nd image in gallery, used file should be "26357237683_0891e46ba5_k.jpg".');
   }
 
@@ -98,16 +98,16 @@ JS;
 
     $this->clickSave();
 
-    $this->clickCssSelector('div.slick--initialized button.slick-next');
+    $this->clickCssSelector('div.slick--initialized.slick--field-media-images button.slick-next');
 
     // Check that, there are 4 images in gallery.
     $numberOfImages = $this->getSession()
-      ->evaluateScript('jQuery(\'div.slick--initialized div.slick-slide:not(.slick-cloned)\').length;');
+      ->evaluateScript('jQuery(\'div.slick--initialized.slick--field-media-images div.slick-slide:not(.slick-cloned)\').length;');
     $this->assertEquals(4, $numberOfImages, 'There should be 4 images in Gallery.');
 
     // Check that, 2nd image is file: 26315068204_24ffa6cfc4_o.jpg.
     $fileNamePosition = $this->getSession()
-      ->evaluateScript('jQuery(\'div.slick--initialized div.slick-slide:not(.slick-cloned):nth(1) img\').attr(\'src\').indexOf("26315068204_24ffa6cfc4_o.jpg")');
+      ->evaluateScript('jQuery(\'div.slick--initialized.slick--field-media-images div.slick-slide:not(.slick-cloned):nth(1) img\').attr(\'src\').indexOf("26315068204_24ffa6cfc4_o.jpg")');
     $this->assertNotEquals(-1, $fileNamePosition, 'For 2nd image in gallery, used file should be "26315068204_24ffa6cfc4_o.jpg".');
 
     // Test add + reorder inside media library.
@@ -129,15 +129,15 @@ JS;
 
     // Check that, there are 5 images in gallery.
     $numberOfImages = $this->getSession()
-      ->evaluateScript('jQuery(\'div.slick--initialized div.slick-slide:not(.slick-cloned)\').length;');
+      ->evaluateScript('jQuery(\'div.slick--initialized.slick--field-media-images div.slick-slide:not(.slick-cloned)\').length;');
     $this->assertEquals(5, $numberOfImages, 'There should be 5 images in Gallery.');
 
-    $this->clickCssSelector('div.slick--initialized button.slick-next');
-    $this->clickCssSelector('div.slick--initialized button.slick-next');
+    $this->clickCssSelector('div.slick--initialized.slick--field-media-images button.slick-next');
+    $this->clickCssSelector('div.slick--initialized.slick--field-media-images button.slick-next');
 
     // Check that, 3rd image is file: reference.jpg.
     $fileNamePosition = $this->getSession()
-      ->evaluateScript('jQuery(\'div.slick--initialized div.slick-slide:not(.slick-cloned):nth(2) img\').attr(\'src\').indexOf("reference.jpg")');
+      ->evaluateScript('jQuery(\'div.slick--initialized.slick--field-media-images div.slick-slide:not(.slick-cloned):nth(2) img\').attr(\'src\').indexOf("reference.jpg")');
     $this->assertNotEquals(-1, $fileNamePosition, 'For 3rd image in gallery, used file should be "reference.jpg".');
 
     // Test remove inside media library.
@@ -157,15 +157,15 @@ JS;
 
     // Check that, there are 4 images in gallery.
     $numberOfImages = $this->getSession()
-      ->evaluateScript('jQuery(\'div.slick--initialized div.slick-slide:not(.slick-cloned)\').length;');
+      ->evaluateScript('jQuery(\'div.slick--initialized.slick--field-media-images div.slick-slide:not(.slick-cloned)\').length;');
     $this->assertEquals(4, $numberOfImages, 'There should be 4 images in Gallery.');
 
-    $this->clickCssSelector('div.slick--initialized button.slick-next');
-    $this->clickCssSelector('div.slick--initialized button.slick-next');
+    $this->clickCssSelector('div.slick--initialized.slick--field-media-images button.slick-next');
+    $this->clickCssSelector('div.slick--initialized.slick--field-media-images button.slick-next');
 
     // Check that, 3rd image is not file: reference.jpg.
     $fileNamePosition = $this->getSession()
-      ->evaluateScript('jQuery(\'div.slick--initialized div.slick-slide:not(.slick-cloned):nth(2) img\').attr(\'src\').indexOf("reference.jpg")');
+      ->evaluateScript('jQuery(\'div.slick--initialized.slick--field-media-images div.slick-slide:not(.slick-cloned):nth(2) img\').attr(\'src\').indexOf("reference.jpg")');
     $this->assertEquals(-1, $fileNamePosition, 'For 2nd image in gallery, used file should not be "reference.jpg".');
   }
 
