c30600b992460201f2a9537735bfbc0203eb558c
Christian Fritsch <chr.fritsch@gmx.net>
2023-12-19 09:22:43 +0100

    Dynamic Entity Browser migration
    * Dynamic entity browser field widget migration
    
    * Improve
    
    * Switch to select
    
    * Fix
    
    * Simplify
    
    * Fix
    
    ---------
    
    Co-authored-by: Christian Fritsch <christian.fritsch@burda.com>

--- thunder.post_update.php
+++ thunder.post_update.php
@@ -5,6 +5,7 @@
  * Update functions for the thunder installation profile.
  */
 
+use Drupal\Core\Entity\Entity\EntityFormDisplay;
 use Drupal\entity_browser\Entity\EntityBrowser;
 use Drupal\user\Entity\Role;
 
@@ -38,6 +39,34 @@ function thunder_post_update_0001_upgrade_to_thunder7(array &$sandbox): string {
       }
     }
     $role->save();
+  }
+
+  foreach (EntityFormDisplay::loadMultiple() as $entity_form_display) {
+    $field_definitions = \Drupal::service('entity_field.manager')->getFieldDefinitions($entity_form_display->getTargetEntityTypeId(), $entity_form_display->getTargetBundle());
+    foreach ($entity_form_display->getComponents() as $component_name => $component) {
+      if (!isset($field_definitions[$component_name])) {
+        continue;
+      }
+      /** @var \Drupal\Core\Field\FieldDefinitionInterface $field_definition */
+      $field_definition = $field_definitions[$component_name];
+      if ($component['type'] === 'entity_browser_entity_reference' && $field_definition->getFieldStorageDefinition()->getSetting('target_type') === 'media') {
+        $multiple = $field_definition->getFieldStorageDefinition()->getCardinality() !== 1;
+        $component['type'] = 'media_library_media_modify_widget';
+        $component['settings'] = [
+          'add_button_text' => Drupal::translation()->formatPlural($multiple ? 2 : 1, 'Select @label', 'Select @labels', [
+            '@label' => 'media item',
+            '@labels' => 'media items',
+          ]),
+          'check_selected' => $multiple,
+          'form_mode' => 'override',
+          'no_edit_on_create' => $multiple,
+          'multi_edit_on_create' => FALSE,
+          'replace_checkbox_by_order_indicator' => $multiple,
+        ];
+        $entity_form_display->setComponent($component_name, $component);
+      }
+      $entity_form_display->save();
+    }
   }
 
   /** @var \Drupal\Core\Extension\ModuleInstallerInterface $moduleInstaller */
