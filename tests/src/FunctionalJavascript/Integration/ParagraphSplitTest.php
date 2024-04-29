b1d261ec9e560f065534b5f8d1e171003ea121cb
Volker Killesreiter <killesre@gmail.com>
2024-04-29 18:17:00 +0200

    align tests

--- tests/src/FunctionalJavascript/Integration/ParagraphSplitTest.php
+++ tests/src/FunctionalJavascript/Integration/ParagraphSplitTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\thunder\FunctionalJavascript\Integration;
 
+use Drupal\Tests\paragraphs_features\FunctionalJavascript\ParagraphsFeaturesJavascriptTestBase;
 use Drupal\Tests\thunder\FunctionalJavascript\ThunderArticleTestTrait;
 use Drupal\Tests\thunder\FunctionalJavascript\ThunderJavascriptTestBase;
 use Drupal\Tests\thunder\FunctionalJavascript\ThunderParagraphsTestTrait;
@@ -40,21 +41,19 @@ class ParagraphSplitTest extends ThunderJavascriptTestBase {
     $firstParagraphContent = '<p>Content that will be in the first paragraph after the split.</p>';
     $secondParagraphContent = '<p>Content that will be in the second paragraph after the split.</p>';
 
-    $this->articleFillNew([]);
-
+    $this->drupalGet("node/add/article");
     // Add text paragraph with two elements.
     $this->addTextParagraph(static::$paragraphsField, $firstParagraphContent . $secondParagraphContent);
 
     // Select second element in editor.
-    $this->selectCkEditorElement($this->getCkEditorCssSelector(0), 1);
-
+    $ck_editor_id = $this->getCkEditorId($this->getCkEditorCssSelector(0));
+    $this->setEditorSelection($ck_editor_id, 'second');
     // Split text paragraph before the current selection.
-    $this->clickParagraphSplitButton();
-    $this->assertSession()->assertWaitOnAjaxRequest();
+    $this->clickParagraphSplitButton($ck_editor_id);
 
-    // Test if all texts are in the correct paragraph.
-    $this->assertCkEditorContent($this->getCkEditorCssSelector(0), $firstParagraphContent . PHP_EOL);
-    $this->assertCkEditorContent($this->getCkEditorCssSelector(1), $secondParagraphContent . PHP_EOL);
+    // Validate split results.
+    $this->assertCkEditorContent($this->getCkEditorCssSelector(0), $firstParagraphContent);
+    $this->assertCkEditorContent($this->getCkEditorCssSelector(1), $secondParagraphContent);
   }
 
   /**
@@ -64,7 +63,7 @@ class ParagraphSplitTest extends ThunderJavascriptTestBase {
     $firstParagraphContent = '<p>Content that will be in the first paragraph after the split.</p>';
     $secondParagraphContent = '<p>Content that will be in the second paragraph after the split.</p>';
 
-    $this->articleFillNew([]);
+    $this->drupalGet("node/add/article");
 
     // Create first paragraph.
     $this->addTextParagraph(static::$paragraphsField, '');
@@ -76,17 +75,16 @@ class ParagraphSplitTest extends ThunderJavascriptTestBase {
 
     // Create second paragraph.
     $this->addTextParagraph(static::$paragraphsField, $firstParagraphContent . $secondParagraphContent);
-
     // Select second element in editor.
-    $this->selectCkEditorElement($this->getCkEditorCssSelector(1), 1);
+    $ck_editor_id = $this->getCkEditorId($this->getCkEditorCssSelector(1));
+    $this->setEditorSelection($ck_editor_id, 'second');
 
     // Split text paragraph.
-    $this->clickParagraphSplitButton();
-    $this->assertSession()->assertWaitOnAjaxRequest();
+    $this->clickParagraphSplitButton($ck_editor_id);
 
     // Test if all texts are in the correct paragraph.
-    $this->assertCkEditorContent($this->getCkEditorCssSelector(1), $firstParagraphContent . PHP_EOL);
-    $this->assertCkEditorContent($this->getCkEditorCssSelector(2), $secondParagraphContent . PHP_EOL);
+    $this->assertCkEditorContent($this->getCkEditorCssSelector(1), $firstParagraphContent);
+    $this->assertCkEditorContent($this->getCkEditorCssSelector(2), $secondParagraphContent);
   }
 
   /**
@@ -97,17 +95,17 @@ class ParagraphSplitTest extends ThunderJavascriptTestBase {
     $secondParagraphContent = '<p>Content that will be in the second paragraph after the split.</p>';
     $thirdParagraphContent = '<p>Content that will be placed into the first paragraph after split.</p>';
 
-    $this->articleFillNew([]);
+    $this->drupalGet("node/add/article");
 
     // Create first paragraph.
     $this->addTextParagraph(static::$paragraphsField, $firstParagraphContent . $secondParagraphContent);
 
     // Select second element in editor.
-    $this->selectCkEditorElement($this->getCkEditorCssSelector(0), 1);
+    $ck_editor_id = $this->getCkEditorId($this->getCkEditorCssSelector(0));
+    $this->setEditorSelection($ck_editor_id, 'second');
 
     // Split text paragraph.
-    $this->clickParagraphSplitButton();
-    $this->assertSession()->assertWaitOnAjaxRequest();
+    $this->clickParagraphSplitButton($ck_editor_id);
 
     $paragraphDelta = $this->getParagraphDelta(static::$paragraphsField, 0);
     $ckEditorCssSelector = "textarea[name='" . static::$paragraphsField . "[{$paragraphDelta}][subform][field_text][0][value]']";
@@ -120,21 +118,45 @@ class ParagraphSplitTest extends ThunderJavascriptTestBase {
     $ckEditorId = $this->getCkEditorId($ckEditorCssSelector);
     $this->getSession()
       ->getDriver()
-      ->executeScript("window.ed = CKEDITOR.instances[\"$ckEditorId\"]; window.ed.setData(\"$thirdParagraphContent\"); window.ed.updateElement(); window.ed.element.data('editor-value-is-changed', true);");
+      ->executeScript("const editor = Drupal.CKEditor5Instances.get('$ckEditorId'); editor.setData(\"$thirdParagraphContent\"); // window.ed.updateElement(); // window.ed.element.data('editor-value-is-changed', true);");
 
     $this->addTextParagraph(static::$paragraphsField, '', 'text', 1);
 
     // Test if all texts are in the correct paragraph.
-    $this->assertCkEditorContent($this->getCkEditorCssSelector(0), $thirdParagraphContent . PHP_EOL);
+    $this->assertCkEditorContent($this->getCkEditorCssSelector(0), $thirdParagraphContent);
     $this->assertCkEditorContent($this->getCkEditorCssSelector(2), '');
-    $this->assertCkEditorContent($this->getCkEditorCssSelector(1), $secondParagraphContent . PHP_EOL);
+    $this->assertCkEditorContent($this->getCkEditorCssSelector(1), $secondParagraphContent);
   }
 
   /**
-   * Click on split button.
+   * Assert that CKEditor instance contains correct data.
+   *
+   * @param string $ckEditorCssSelector
+   *   CSS selector for CKEditor.
+   * @param string $expectedContent
+   *   The expected content.
    */
-  protected function clickParagraphSplitButton() {
-    $this->getSession()->executeScript("jQuery('.cke_button__splittext')[0].click();");
+  public function assertCkEditorContent($ckEditorCssSelector, $expectedContent) {
+    $ckEditorId = $this->getCkEditorId($ckEditorCssSelector);
+    $ckEditorContent = $this->getSession()
+      ->getDriver()
+      ->evaluateScript("Drupal.CKEditor5Instances.get('$ckEditorId').getData();");
+
+    static::assertEquals($expectedContent, $ckEditorContent);
+  }
+
+  /**
+   * Click on split text button for paragraphs text field.
+   *
+   * @param int $ck_editor_id
+   *   Id of CKEditor field in paragraphs.
+   */
+  protected function clickParagraphSplitButton($ck_editor_id): void {
+    $button = $this->assertSession()
+      ->waitForElementVisible('xpath', '//textarea[@data-ckeditor5-id="' . $ck_editor_id . '"]/following-sibling::div//button[span[text()="Split Paragraph"]]');
+    $this->assertNotEmpty($button);
+    $button->click();
+    $this->assertSession()->assertWaitOnAjaxRequest();
   }
 
   /**
@@ -148,6 +170,39 @@ class ParagraphSplitTest extends ThunderJavascriptTestBase {
    */
   protected function getCkEditorCssSelector($paragraphDelta) {
     return sprintf(static::$selectorTemplate, static::$paragraphsField, $paragraphDelta);
+  }
+
+  /**
+   * Set selection to beginning of an element containing a given string.
+   *
+   * @param int $ck_editor_id
+   *   Id of CKEditor field in paragraphs.
+   * @param string $needle
+   *   String contained by element.
+   */
+  protected function setEditorSelection($ck_editor_id, $needle): void {
+    $script = <<<JS
+(function (editorId, needle) {
+  const editor = Drupal.CKEditor5Instances.get(editorId);
+  editor.model.change( writer => {
+    const selection = writer.createSelection(editor.model.document.getRoot(), 'in');
+    let newPosition;
+    for (const range of selection.getRanges()) {
+      for (const item of range.getItems()) {
+        if (item.data?.includes(needle)) {
+          newPosition = writer.createPositionAt(item, 'before');
+          break;
+        }
+      }
+    }
+    const newRange = writer.createRange( newPosition );
+    writer.setSelection( newRange );
+    editor.focus()
+  });
+})('{$ck_editor_id}', '{$needle}')
+JS;
+
+    $this->getSession()->getDriver()->executeScript($script);
   }
 
 }
