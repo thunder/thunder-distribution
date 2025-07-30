fd4111eb705f85c4bbc71ca78f58108de8e4c76a
Daniel Bosen <daniel@bosen.eu>
2025-07-30 08:18:04 +0200

    Add types

--- src/Hook/Form.php
+++ src/Hook/Form.php
@@ -14,7 +14,7 @@ class Form {
    * Implements hook_form_alter().
    */
   #[Hook('form_alter')]
-  public function formAlter(&$form, FormStateInterface $form_state, $form_id): void {
+  public function formAlter(array &$form, FormStateInterface $form_state, string $form_id): void {
     // Move content lock unlock button to the more actions array.
     if (!empty($form['actions']['unlock']['#gin_action_item'])) {
       $form['actions']['unlock']['#gin_action_item'] = FALSE;
