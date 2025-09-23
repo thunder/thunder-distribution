db93b299be01b27460869611aadbdc04eb7d3de5
Daniel Bosen <daniel.bosen@burda.com>
2025-09-23 17:29:38 +0200

    Breadcrumb builder refactor

--- modules/thunder_news_article/src/Breadcrumb/ThunderNewsArticleBreadcrumbBuilder.php
+++ modules/thunder_news_article/src/Breadcrumb/ThunderNewsArticleBreadcrumbBuilder.php
@@ -1,23 +1,24 @@
 <?php
 
-namespace Drupal\thunder_article\Breadcrumb;
+namespace Drupal\thunder_news_article\Breadcrumb;
 
 use Drupal\Core\Breadcrumb\Breadcrumb;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\taxonomy\TermInterface;
+use Drupal\thunder_article\Breadcrumb\ThunderTaxonomyTermBreadcrumbBuilderBase;
 
 /**
  * Class to define the menu_link breadcrumb builder.
  */
-class ThunderArticleBreadcrumbBuilder extends ThunderTaxonomyTermBreadcrumbBuilderBase {
+class ThunderNewsArticleBreadcrumbBuilder extends ThunderTaxonomyTermBreadcrumbBuilderBase {
 
   /**
    * {@inheritdoc}
    */
   public function applies(RouteMatchInterface $route_match): bool {
-    // This breadcrumb apply only for all articles.
+    // This breadcrumb apply only for all news articles.
     $parameters = $route_match->getParameters()->all();
-    return ($route_match->getRouteName() === 'entity.node.canonical') && is_object($parameters['node']) && $parameters['node']->getType() === 'article' && !empty($parameters['node']->field_channel->entity);
+    return ($route_match->getRouteName() === 'entity.node.canonical') && is_object($parameters['node']) && $parameters['node']->getType() === 'news_article' && !empty($parameters['node']->field_channel->entity);
   }
 
   /**
