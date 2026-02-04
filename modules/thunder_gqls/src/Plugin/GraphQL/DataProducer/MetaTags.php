d5cc9eb82497458297c49bc2471d98505c4e54a3
Volker Killesreiter <killesre@gmail.com>
2026-02-04 17:18:20 +0100

    Fix node/add preview

--- modules/thunder_gqls/src/Plugin/GraphQL/DataProducer/MetaTags.php
+++ modules/thunder_gqls/src/Plugin/GraphQL/DataProducer/MetaTags.php
@@ -96,7 +96,7 @@ class MetaTags extends DataProducerPluginBase implements ContainerFactoryPluginI
    *   Normalized metatags.
    */
   public function resolve(mixed $value, ?string $type, RefinableCacheableDependencyInterface $metadata): array {
-    if ($value instanceof ContentEntityInterface) {
+    if ($value instanceof ContentEntityInterface && !$value->isNew()) {
       $context = new RenderContext();
       $result = $this->renderer->executeInRenderContext($context, function () use ($value): array {
         $tags = $this->metatagManager->tagsFromEntityWithDefaults($value);
