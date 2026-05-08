93d3a05bfc1e568118fd0379ba8b67f979a0de4b
Ahmad Hassan <ahmad.hassan@burda.com>
2026-05-08 15:45:13 +0200

    feature/addmore-key attribute.

--- modules/thunder_gqls/src/Plugin/GraphQL/DataProducer/ThunderEntityListHasNext.php
+++ modules/thunder_gqls/src/Plugin/GraphQL/DataProducer/ThunderEntityListHasNext.php
@@ -2,27 +2,25 @@
 
 namespace Drupal\thunder_gqls\Plugin\GraphQL\DataProducer;
 
+use Drupal\Core\Plugin\Context\ContextDefinition;
+use Drupal\graphql\Attribute\DataProducer;
 use Drupal\graphql\Plugin\GraphQL\DataProducer\DataProducerPluginBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\thunder_gqls\Wrappers\EntityListResponseHasNextInterface;
 use Drupal\thunder_gqls\Wrappers\EntityListResponseInterface;
 
 /**
  * Checks if there are more items beyond the current page.
- *
- * @DataProducer(
- *   id = "thunder_entity_list_has_next",
- *   name = @Translation("Entity list has next"),
- *   description = @Translation("Whether there are more items beyond the current page."),
- *   produces = @ContextDefinition("boolean",
- *     label = @Translation("Has next")
- *   ),
- *   consumes = {
- *     "entityList" = @ContextDefinition("any",
- *       label = @Translation("Entity list response")
- *     ),
- *   }
- * )
  */
+#[DataProducer(
+  id: 'thunder_entity_list_has_next',
+  name: new TranslatableMarkup('Entity list has next'),
+  description: new TranslatableMarkup('Whether there are more items beyond the current page.'),
+  produces: new ContextDefinition('boolean', label: new TranslatableMarkup('Has next')),
+  consumes: [
+    'entityList' => new ContextDefinition('any', label: new TranslatableMarkup('Entity list response')),
+  ]
+)]
 class ThunderEntityListHasNext extends DataProducerPluginBase {
 
   /**
