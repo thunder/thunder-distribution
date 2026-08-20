633bb06970c70021a89560621c0769e6ec5627cb
Daniel Bosen <daniel.bosen@burda.com>
2026-08-20 15:41:39 +0200

    OOP version of hook requirements

---
+++ modules/thunder_media/src/Hook/Requirements.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\thunder_media\Hook;
+
+use Drupal\Core\Extension\Requirement\RequirementSeverity;
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\Process\ExecutableFinder;
+
+/**
+ * Runtime requirements hooks for the thunder_media module.
+ */
+class Requirements {
+
+  use StringTranslationTrait;
+
+  /**
+   * Implements hook_runtime_requirements().
+   */
+  #[Hook('runtime_requirements')]
+  public function runtimeRequirements(): array {
+    $requirements = [];
+
+    if ((new ExecutableFinder())->find('exiftool') === NULL) {
+      $requirements['thunder_media_exiftool'] = [
+        'title' => $this->t('Thunder Media: exiftool'),
+        'value' => $this->t('Not found'),
+        'description' => $this->t('The "exiftool" binary was not found on PATH. Images flagged via the "AI disclosure" field will not have AI-disclosure metadata embedded until it is installed.'),
+        'severity' => RequirementSeverity::Warning,
+      ];
+    }
+
+    return $requirements;
+  }
+
+}
