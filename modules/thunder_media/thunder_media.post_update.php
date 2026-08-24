2cd8c3c2cea105d171e964f11eb2b55533bae16c
Daniel Bosen <daniel.bosen@burda.com>
2026-08-24 16:12:06 +0200

    use update helper for settins

--- modules/thunder_media/thunder_media.post_update.php
+++ modules/thunder_media/thunder_media.post_update.php
@@ -27,15 +27,16 @@ function thunder_media_post_update_filename_transliteration(): void {
 /**
  * Add the "AI disclosure" settings.
  */
-function thunder_media_post_update_add_ai_disclosure_settings(): void {
-  $config = \Drupal::configFactory()->getEditable('thunder_media.settings');
+function thunder_media_post_update_add_ai_disclosure_settings(): string {
+  /** @var \Drupal\update_helper\Updater $updater */
+  $updater = \Drupal::service('update_helper.updater');
 
-  if ($config->get('ai_disclosure_upload_only') === NULL) {
-    $config->set('ai_disclosure_upload_only', FALSE);
+  try {
+    $updater->executeUpdate('thunder_media', 'thunder_media_post_update_add_ai_disclosure_settings');
   }
-  if ($config->get('ai_disclosure_auto_detect') === NULL) {
-    $config->set('ai_disclosure_auto_detect', TRUE);
+  catch (\Exception $e) {
+    \Drupal::logger('thunder_media')->warning('Could not add the AI disclosure settings: @message', ['@message' => $e->getMessage()]);
   }
 
-  $config->save();
+  return $updater->logger()->output();
 }
